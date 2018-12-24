b04deeab319778a7686dd91e315fa20ca564f21c
Thanh Doan <tmdoan98@gmail.com>
2018-12-24 23:25:39 +0000

    moved normalization to actions

--- client/src/components/group/duck/actions.js
+++ client/src/components/group/duck/actions.js
@@ -1,18 +1,36 @@
 import { createAction } from "redux-actions";
-import * as types from "./types"
+import { normalize } from "normalizr";
+import schemas from "../../../schemas";
 
+import {
+    GROUP_CREATE_REQUEST,
+    GROUP_CREATE_SUCCESS,
+    GROUP_CREATE_FAILURE,
+
+    GROUP_UPDATE_REQUEST,
+    GROUP_UPDATE_SUCCESS,
+    GROUP_UPDATE_FAILURE,
+
+    GROUP_DELETE_REQUEST,
+    GROUP_DELETE_SUCCESS,
+    GROUP_DELETE_FAILURE,
+
+    GROUP_SELECT
+} from "./types";
+
+const payloadCreator = (data) => normalize(data, schemas.group);
 const metaCreator = () => ({ errorType: "group" });
 
-export const createGroupRequest = createAction(types.GROUP_CREATE_REQUEST);
-export const createGroupSuccess = createAction(types.GROUP_CREATE_SUCCESS);
-export const createGroupFailure = createAction(types.GROUP_CREATE_FAILURE, null, metaCreator);
+export const createGroupRequest = createAction(GROUP_CREATE_REQUEST);
+export const createGroupSuccess = createAction(GROUP_CREATE_SUCCESS, payloadCreator);
+export const createGroupFailure = createAction(GROUP_CREATE_FAILURE, null, metaCreator);
 
-export const updateGroupRequest = createAction(types.GROUP_UPDATE_REQUEST);
-export const updateGroupSuccess = createAction(types.GROUP_UPDATE_SUCCESS);
-export const updateGroupFailure = createAction(types.GROUP_UPDATE_FAILURE, null, metaCreator);
+export const updateGroupRequest = createAction(GROUP_UPDATE_REQUEST);
+export const updateGroupSuccess = createAction(GROUP_UPDATE_SUCCESS, payloadCreator);
+export const updateGroupFailure = createAction(GROUP_UPDATE_FAILURE, null, metaCreator);
 
-export const deleteGroupRequest = createAction(types.GROUP_DELETE_REQUEST);
-export const deleteGroupSuccess = createAction(types.GROUP_DELETE_SUCCESS);
-export const deleteGroupFailure = createAction(types.GROUP_DELETE_FAILURE, null, metaCreator);
+export const deleteGroupRequest = createAction(GROUP_DELETE_REQUEST);
+export const deleteGroupSuccess = createAction(GROUP_DELETE_SUCCESS, payloadCreator);
+export const deleteGroupFailure = createAction(GROUP_DELETE_FAILURE, null, metaCreator);
 
-export const selectGroup = createAction(types.GROUP_SELECT);
+export const selectGroup = createAction(GROUP_SELECT, _id => payloadCreator({ _id }));
