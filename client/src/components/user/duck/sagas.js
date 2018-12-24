7b079eedf8072bf1d033da04f8f42ef4fc3d81b1
Thanh Doan <tmdoan98@gmail.com>
2018-12-24 23:21:52 +0000

    moved handling of user data to the user saga

--- client/src/components/user/duck/sagas.js
+++ client/src/components/user/duck/sagas.js
@@ -1,37 +1,27 @@
-// import { take, put, call, select } from "redux-saga/effects";
-// import { USER_LOAD_INITIAL_DATA_REQUEST } from "./types";
-// import userApi from "../../../api/userApi";
-// import { normalizeInitialState } from "../../../schemas/normalize";
-// import { 
-//     loadInitialUserDataFailure, 
-//     loadInitialUserDataSuccess,
-// } from "./actions";
-// import { selectGroup } from "../../group/duck/actions";
-// import { loadAppSuccess, loadAppFailure } from "../../app/duck/actions";
-// import { getCurrentGroupDefault } from "../../group/duck/selectors";
-// import { showAuthentication } from "../../auth/duck/actions";
+import { take, put, call, select } from "redux-saga/effects";
+import { normalize } from "normalizr";
+import schemas from "../../../schemas";
 
-// export function* loadInitialUserData() {
-//     while(true) {
-//         yield take(USER_LOAD_INITIAL_DATA_REQUEST);
+import { loadUserDataSuccess } from "./actions";
 
-//         try {
-//             const { data } = yield call(userApi.getCurrentUser);
-//             //console.log(data);
-//             if (!data) return
-//             const normalizedData = yield call(normalizeInitialState, data);
-//             yield put(loadInitialUserDataSuccess(normalizedData));
-//             const currentGroupDefault = yield select(getCurrentGroupDefault);
-//             console.log("cgDEFAYKT", currentGroupDefault);
-//             yield put(selectGroup(currentGroupDefault));
-//             yield put(loadAppSuccess());
-//         } catch (error) {
-//             console.log(error);
-//             //error.response.data
-//             yield put(loadInitialUserDataFailure(error));
-//             yield put(loadAppFailure());
-//             //yield put(showAuthentication());
-//             return;
-//         }
-//     }
-// }
+import { 
+    AUTH_JWT_SUCCESS,
+    AUTH_LOGIN_SUCCESS,
+    AUTH_REGISTRATION_SUCCESS
+} from "../../auth/duck/types";
+
+export function* watchAuthenticationSuccess() {
+    while(true) {
+        const { payload } = yield take([
+            AUTH_JWT_SUCCESS,
+            AUTH_LOGIN_SUCCESS,
+            AUTH_REGISTRATION_SUCCESS
+        ]);
+        yield call(handleUserDataLoad, payload);
+    }
+}
+
+function* handleUserDataLoad(data) {
+    console.log("DATA", data);
+    yield put(loadUserDataSuccess(data));
+}
