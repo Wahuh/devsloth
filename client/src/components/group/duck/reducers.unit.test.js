de91e839a1803ca5c5928344e9b4e0e924cd8fdc
Thanh Doan <tmdoan98@gmail.com>
2018-12-24 23:24:32 +0000

    added tests for all group reducers

--- client/src/components/group/duck/reducers.unit.test.js
+++ client/src/components/group/duck/reducers.unit.test.js
@@ -1,184 +1,132 @@
-import { byId, allIds, currentId } from "./reducers";
-import * as actions from "./actions";
+import { byId, allIds, select } from "./reducers";
 
-const createGroupPayload = {
-    "entities": {
-      "members": {
-        "5c1147b3c6e5611ef45229b5": {
-          "channels": [
-            "5c11330955e8631a10b56c95",
-            "5c114af4033247581cf3fe02",
-            "5c114b26191a671ff014284f",
-            "5c114ba2a6d2e1246072ba95",
-            "5c13f898d4399631f4b3287f",
-            "5c13f97cd4399631f4b32881",
-            "5c14ed35e270b13708661a10"
-          ],
-          "_id": "5c1147b3c6e5611ef45229b5",
-          "groups": [
-            "5c11330955e8631a10b56c94",
-            null,
-            null,
-            "5c114ba2a6d2e1246072ba94",
-            "5c13f898d4399631f4b3287e",
-            "5c13f97cd4399631f4b32880",
-            "5c14ed35e270b13708661a0f"
-          ]
-        }
-      },
-      "channels": {
-        "5c14ed35e270b13708661a10": {
-          "_id": "5c14ed35e270b13708661a10",
-          "name": "general",
-          "group": "5c14ed35e270b13708661a0f",
-          "__v": 0,
-          "members": [
-            "5c1147b3c6e5611ef45229b5"
-          ],
-          "tasks": null,
-          "id": "5c14ed35e270b13708661a10"
-        }
-      },
-      "groups": {
-        "5c14ed35e270b13708661a0f": {
-          "_id": "5c14ed35e270b13708661a0f",
-          "name": "adad",
-          "__v": 0,
-          "channels": [
-            "5c14ed35e270b13708661a10"
-          ],
-          "members": [
-            "5c1147b3c6e5611ef45229b5"
-          ],
-          "roles": null,
-          "id": "5c14ed35e270b13708661a0f"
-        }
-      }
-    },
-    "result": "5c14ed35e270b13708661a0f"
-}
+import { 
+    createGroupSuccess,
+    deleteGroupSuccess,
+    updateGroupSuccess,
+    selectGroup
+} from "./actions";
+import { loadUserDataSuccess } from "../../user/duck/actions";
 
+import { loadUserAction } from "../../../tests/actions/user";
 
-describe("group reducer", () => {
-    describe("byId", () => {
+import { 
+    createGroupData,
+    deleteGroupData,
+    updateGroupData
+} from "../../../tests/data/group";
+import { userData } from "../../../tests/data/user";
+import { 
+    createGroupAction,
+    updateGroupAction,
+    deleteGroupAction,
+ } from "../../../tests/actions/group";
+
+describe("Group Reducers", () => {
+    describe("byId (Group)", () => {
         it("should return the initial state", () => {
             expect(byId(undefined, {})).toEqual({});
-        })
+        });
 
         it("handles creating a group", () => {
-            expect(
-                byId({}, actions.createGroupSuccess(createGroupPayload))
-            )
-            .toEqual({
-                "5c14ed35e270b13708661a0f": {
-                    "_id": "5c14ed35e270b13708661a0f",
-                    "name": "adad",
-                    "__v": 0,
-                    "channels": [
-                        "5c14ed35e270b13708661a10"
-                    ],
-                    "members": [
-                        "5c1147b3c6e5611ef45229b5"
-                    ],
-                    "roles": null,
-                    "id": "5c14ed35e270b13708661a0f"
-                }
-            });
+            expect(byId({}, createGroupSuccess(createGroupData)))
+            .toEqual(createGroupAction.payload.entities.groups);
         });
 
         it("handles deleting a group", () => {
             expect(
-                byId({
-                    "5c14ed35e270b13708661a0f": {
-                        "_id": "5c14ed35e270b13708661a0f",
-                        "name": "adad",
-                        "__v": 0,
-                        "channels": [
-                            "5c14ed35e270b13708661a10"
-                        ],
-                        "members": [
-                            "5c1147b3c6e5611ef45229b5"
-                        ],
-                        "roles": null,
-                        "id": "5c14ed35e270b13708661a0f"
-                    }
-                }, actions.deleteGroupSuccess({ _id: "5c14ed35e270b13708661a0f" }))
+                byId(
+                    createGroupAction.payload.entities.groups, 
+                    deleteGroupSuccess(deleteGroupData))
             ).toEqual({})
         });
 
         it("handles updating a group", () => {
             expect(
-                byId({
-                    "5c14ed35e270b13708661a0f": {
-                        "_id": "5c14ed35e270b13708661a0f",
-                        "name": "adad",
-                        "__v": 0,
-                        "channels": [
-                            "5c14ed35e270b13708661a10"
-                        ],
-                        "members": [
-                            "5c1147b3c6e5611ef45229b5"
-                        ],
-                        "roles": null,
-                        "id": "5c14ed35e270b13708661a0f"
-                    }  
-                }, actions.updateGroupSuccess({}))
-            ).toEqual({
-                "5c14ed35e270b13708661a0f": {
-                    "_id": "5c14ed35e270b13708661a0f",
-                    "name": "changed",
-                    "__v": 0,
-                    "channels": [
-                        "5c14ed35e270b13708661a10"
-                    ],
-                    "members": [
-                        "5c1147b3c6e5611ef45229b5"
-                    ],
-                    "roles": null,
-                    "id": "5c14ed35e270b13708661a0f"
-                }
-            })
+                byId(createGroupAction.payload.entities.groups, 
+                    updateGroupSuccess(updateGroupData))
+            ).toEqual(updateGroupAction.payload.entities.groups)
+        });
+
+        it("handles loading a user's groups", () => {
+            expect(
+                byId({}, loadUserDataSuccess(userData))
+            ).toEqual(loadUserAction.payload.entities.groups)
         });
     });
 
-    describe("allIds", () => {
+    describe("allIds (Group)", () => {
         it("should return the initial state", () => {
             expect(allIds(undefined, {})).toEqual([]);
         });
 
         it("handles creating a group", () => {
-            expect(allIds([], actions.createGroupSuccess(createGroupPayload)))
-            .toEqual(["5c14ed35e270b13708661a0f"]);
+            expect(allIds([], createGroupSuccess(createGroupData)))
+            .toEqual(Object.keys(createGroupAction.payload.entities.groups));
         });
 
         it("handles deleting a group", () => {
             expect(
-                allIds(["5c14ed35e270b13708661a0f"], actions.deleteGroupSuccess({ _id: "5c14ed35e270b13708661a0f"}))
+                allIds(Object.keys(createGroupAction.payload.entities.groups), 
+                deleteGroupSuccess(deleteGroupData))
             ).toEqual([])
-        })
+        });
+
+        it("handles loading a user's groups", () => {
+            expect(allIds([], loadUserDataSuccess(userData))
+            ).toEqual(Object.keys(loadUserAction.payload.entities.groups))
+        });
     });
 
-    describe("currentId", () => {
+    describe("select (Group)", () => {
+        const initialState = { currentId: null, previousIds: [] }
         it("should return the initial state", () => {
-            expect(currentId(undefined, {})).toEqual(null);
+            expect(select(undefined, {})).toEqual(initialState);
         });
 
         it("handles selecting a group", () => {
             expect(
-                currentId(null, actions.selectGroup("5c14ed35e270b13708661a0f"))
-            ).toEqual("5c14ed35e270b13708661a0f")
+                select(initialState, selectGroup("5c14ed35e270b13708661a0f"))
+            ).toEqual({ currentId:"5c14ed35e270b13708661a0f", previousIds: [] })
         });
 
-        it("changes to the id of the new group if one is created", () => {
+        it("handles changing to the previous group if the current group is deleted", () => {
             expect(
-                currentId("anyId", actions.createGroupSuccess(createGroupPayload))
-            ).toEqual("5c14ed35e270b13708661a0f")
+                select({ currentId: deleteGroupAction.payload.result, previousIds: ["anotherId", "previousId"] }, deleteGroupSuccess(deleteGroupData))
+            ).toEqual({ currentId: "previousId", previousIds: ["anotherId"] });
         });
 
-        it("returns null if the current group is deleted", () => {
+        it("returns currentId as null if the current group is deleted and there are no previous groups", () => {
             expect(
-                currentId("5c14ed35e270b13708661a0f", actions.deleteGroupSuccess({ _id: "5c14ed35e270b13708661a0f" }))
-            ).toEqual(null)
+                select({ currentId: deleteGroupAction.payload.result, previousIds: [] }, deleteGroupSuccess(deleteGroupData))
+            ).toEqual({ currentId: null, previousIds: [] });   
+        });
+
+        it("handles selecting a newly created group", () => {
+            expect(
+                select({
+                    currentId: "anyId",
+                    previousIds: []
+                }, createGroupSuccess(createGroupData))
+            ).toEqual({ 
+                currentId: createGroupAction.payload.result, 
+                previousIds: ["anyId"] 
+            });
+        });
+
+        it("handles loading a user's groups", () => {
+            expect(
+                select(initialState, loadUserDataSuccess(userData))
+            ).toEqual({ currentId: Object.keys(loadUserAction.payload.entities.groups)[0], previousIds: [] })
+        });
+
+        it("removes deleted groups from previousIds", () => {
+            expect(
+                select(
+                    { currentId: "anyId", previousIds: [deleteGroupAction.payload.result, "anotherId"] },
+                    deleteGroupSuccess(deleteGroupData)
+                )
+            ).toEqual({ currentId: "anyId", previousIds: ["anotherId"] })
         });
     });
 });
