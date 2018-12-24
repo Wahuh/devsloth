156fcb6e5a49072ec1c20562f5825dc724b9b8a5
Thanh Doan <tmdoan98@gmail.com>
2018-12-24 23:20:41 +0000

    added mock data for tests

---
+++ client/src/tests/data/channel/index.js
@@ -0,0 +1,22 @@
+export const createChannelData = {
+    "_id": "5c21616fd0d4ef2fd4130f59",
+    "name": "bugs",
+    "group": "5c210edeac006d3318c511a9",
+    "__v": 0,
+    "members": [
+      {
+        "channels": [
+          "5c210edeac006d3318c511aa",
+          "5c21616fd0d4ef2fd4130f59"
+        ],
+        "_id": "5c2110b0d0d4ef2fd4130f55",
+        "user": "5c2110b0d0d4ef2fd4130f54",
+        "group": "5c210edeac006d3318c511a9",
+        "alias": "Tbong",
+        "__v": 0,
+        "id": "5c2110b0d0d4ef2fd4130f55"
+      }
+    ],
+    "tasks": null,
+    "id": "5c21616fd0d4ef2fd4130f59"
+  }
---
+++ client/src/tests/data/group/index.js
@@ -0,0 +1,96 @@
+export const createGroupData = {
+    "_id": "5c2122a4d0d4ef2fd4130f56",
+    "name": "test",
+    "owner": "5c210e0eac98592c3c3ceaed",
+    "inviteId": "NdCFjiRX-",
+    "__v": 0,
+    "channels": [
+        {
+            "_id": "5c2122a4d0d4ef2fd4130f57",
+            "name": "everyone",
+            "group": "5c2122a4d0d4ef2fd4130f56",
+            "__v": 0,
+            "members": [
+                {
+                    "channels": [
+                        "5c2122a4d0d4ef2fd4130f57"
+                    ],
+                    "_id": "5c2122a4d0d4ef2fd4130f58",
+                    "alias": "Wahuh",
+                    "user": "5c210e0eac98592c3c3ceaed",
+                    "group": "5c2122a4d0d4ef2fd4130f56",
+                    "__v": 0,
+                    "id": "5c2122a4d0d4ef2fd4130f58"
+                }
+            ],
+            "tasks": null,
+            "id": "5c2122a4d0d4ef2fd4130f57"
+        }
+    ],
+    "members": [
+        {
+            "channels": [
+                "5c2122a4d0d4ef2fd4130f57"
+            ],
+            "_id": "5c2122a4d0d4ef2fd4130f58",
+            "alias": "Wahuh",
+            "user": "5c210e0eac98592c3c3ceaed",
+            "group": "5c2122a4d0d4ef2fd4130f56",
+            "__v": 0,
+            "id": "5c2122a4d0d4ef2fd4130f58"
+        }
+    ],
+    "roles": [],
+    "id": "5c2122a4d0d4ef2fd4130f56"
+}
+
+export const updateGroupData = {
+    "_id": "5c2122a4d0d4ef2fd4130f56",
+    "name": "name changed",
+    "owner": "5c210e0eac98592c3c3ceaed",
+    "inviteId": "NdCFjiRX-",
+    "__v": 0,
+    "channels": [
+        {
+            "_id": "5c2122a4d0d4ef2fd4130f57",
+            "name": "everyone",
+            "group": "5c2122a4d0d4ef2fd4130f56",
+            "__v": 0,
+            "members": [
+                {
+                    "channels": [
+                        "5c2122a4d0d4ef2fd4130f57"
+                    ],
+                    "_id": "5c2122a4d0d4ef2fd4130f58",
+                    "alias": "Wahuh",
+                    "user": "5c210e0eac98592c3c3ceaed",
+                    "group": "5c2122a4d0d4ef2fd4130f56",
+                    "__v": 0,
+                    "id": "5c2122a4d0d4ef2fd4130f58"
+                }
+            ],
+            "tasks": null,
+            "id": "5c2122a4d0d4ef2fd4130f57"
+        }
+    ],
+    "members": [
+        {
+            "channels": [
+                "5c2122a4d0d4ef2fd4130f57"
+            ],
+            "_id": "5c2122a4d0d4ef2fd4130f58",
+            "alias": "Wahuh",
+            "user": "5c210e0eac98592c3c3ceaed",
+            "group": "5c2122a4d0d4ef2fd4130f56",
+            "__v": 0,
+            "id": "5c2122a4d0d4ef2fd4130f58"
+        }
+    ],
+    "roles": [],
+    "id": "5c2122a4d0d4ef2fd4130f56"
+}
+
+
+export const deleteGroupData = {
+    "_id": "5c2122a4d0d4ef2fd4130f56"
+}
