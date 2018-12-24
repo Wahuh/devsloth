8006b19e373e78739b69203f06b7d549c8624958
Thanh Doan <tmdoan98@gmail.com>
2018-12-24 23:24:17 +0000

    added the select reducer instead of currentId

--- client/src/components/group/duck/reducers.js
+++ client/src/components/group/duck/reducers.js
@@ -8,16 +8,12 @@ import {
     selectGroup,
 } from "./actions";
 
-import { 
-    mergeObject, 
-    mergeArray, 
-    removeFromArray, 
-    removeFromObject 
-} from "../../../utils";
+import { loadUserDataSuccess } from "../../user/duck/actions";
 
 export const byId = handleActions(
     {
-        [createGroupSuccess]: (state, { payload }) => addGroup(state, payload),
+        [loadUserDataSuccess]: (state, { payload }) => addGroups(state, payload),
+        [createGroupSuccess]: (state, { payload }) => addGroups(state, payload),
         [deleteGroupSuccess]: (state, { payload }) => removeGroup(state, payload),
         [updateGroupSuccess]: (state, { payload }) => updateGroup(state, payload),
     }, {}
@@ -25,67 +21,81 @@ export const byId = handleActions(
 
 export const allIds = handleActions(
     {
-        [createGroupSuccess]: (state, { payload }) => addGroupId(state, payload),
+        [loadUserDataSuccess]: (state, { payload }) => addGroupIds(state, payload),
+        [createGroupSuccess]: (state, { payload }) => addGroupIds(state, payload),
         [deleteGroupSuccess]: (state, { payload }) => removeGroupId(state, payload),
     }, []
 );
 
-export const currentId = handleActions(
+export const select = handleActions(
     {
-        [selectGroup]: (state, { payload }) => updateCurrentId(state, payload),
-        [createGroupSuccess]: (state, { payload }) => payload.result,
-        [deleteGroupSuccess]: (state, { payload }) => { 
-            console.log("pp", payload)
-            return payload._id === state ? null : state
-        }
-    }, null
+        [loadUserDataSuccess]: (state, { payload }) => updateSelectFromUserData(state, payload),
+        [createGroupSuccess]: (state, { payload }) => updateSelect(state, payload),
+        [deleteGroupSuccess]: (state, { payload }) => removeFromSelect(state, payload),
+        [selectGroup]: (state, { payload }) => updateSelect(state, payload)
+    }, { currentId: null, previousIds: [] }
 );
 
 
-const addGroup = (state, { entities }) => {
+const addGroups = (state, { entities }) => {
     const { groups } = entities;
-    return mergeObject(state, groups);
+    return { ...state, ...groups };
 }
 
-const addGroupId = (state, { result }) => {
-    return mergeArray(state, result);
+const addGroupIds = (state, { entities }) => {
+    const { groups } = entities;
+    return [ ...state, ...Object.keys(groups) ];
 }
 
-const removeGroup = (state, { _id }) => {
-    return removeFromObject(state, _id);
+const removeGroup = (state, { result: groupId }) => {
+    const { [groupId]: removedGroup, ...rest } = state;
+    return rest;
 }
 
-const removeGroupId = (state, { _id }) => {
-    return removeFromArray(state, _id);
+const removeGroupId = (state, { result: groupId }) => {
+    return state.filter(id => id !== groupId);
 }
 
-const updateGroup = (state, { groups }) => {
-
-    // allids = ["1"] byId={"1": {id: 1}}
-    // const { byId, allIds } = groups;
-    // const group = { byId[allIds[0]] }
-    return mergeObject(state, {})
+const updateGroup = (state, { entities, result: id }) => {
+    const { groups } = entities;
+    const updatedGroup = groups[id];
+    const group = { ...state[id] };
+    group.name = updatedGroup.name;    
+    return { ...state, [id]: group };
 }
 
-const updateCurrentId = (state, { _id }) => {
+const updateSelect = (state, { result: newId }) => {
+    const { currentId, previousIds } = state;
+    if (currentId) {
+        return { currentId: newId, previousIds: [ ...previousIds, currentId ] };
+    }
+    return { currentId: newId, previousIds: [] };
+}
 
-    return _id;
+const updateSelectFromUserData = (state, { entities }) => {
+    const { groups } = entities;
+    return { currentId: Object.keys(groups)[0], previousIds: [] }
+}
+
+const removeFromSelect = (state, { entities, result: deletedId }) => {
+    const select = { ...state };
+    const { currentId, previousIds } = select;
+
+    if (currentId === deletedId) {
+        if (previousIds.length == 0) {
+            select.currentId = null;
+            return select;
+        }
+        select.currentId = select.previousIds.pop();
+        return select;
+    } else if (previousIds.includes(deletedId)) {
+        select.previousIds = previousIds.filter(id => id !== deletedId);
+        return select;
+    }
 }
 
 export default combineReducers({
     byId,
     allIds,
-    currentId
+    select
 });
-
-    //     case types.GROUP_UPDATE_SUCCESS:
-    //     const group = payload;
-    //     return { 
-    //         ...state, 
-    //         [group._id]: { ...state[group._id], ...group }
-    //     };
-
-    // case types.GROUP_DELETE_SUCCESS:
-    //     const groupId = payload;    
-    //     let { [groupId]: deletedItem, ...rest } = state;
-    //     return rest;
