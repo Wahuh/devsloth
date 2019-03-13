444792b86af3d31a3ed16eb204b8c6572a81a9ed
Thanh Doan <tmdoan98@gmail.com>
2019-03-13 22:54:11 +0000

    convert to functional component

--- client/src/components/ui/ThemeSwitcher/index.js
+++ client/src/components/ui/ThemeSwitcher/index.js
@@ -1,40 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "../../reuse/Button";
 import PaletteIcon from "../../reuse/icons/PaletteIcon";
 import styles from "./ThemeSwitcher.scss";
-import themeStyles from "../../../styles/themes.scss";
 import Tooltip from "../../reuse/Tooltip";
+import Icon from "../../reuse/Icon";
+import themeApi from "../../../api/themeApi";
 
-const themes = {
-    light: themeStyles.light,
-    dark: themeStyles.dark
-}
-
-class ThemeSwitcher extends Component {
-    state = {
-        theme: "dark"
-    };
-
-    toggle = (theme) => {
-        const root = document.documentElement;
-        root.className = themes[theme];
-        this.setState({ theme });
-    }
-
-    render() {
-        const { theme } = this.state;
-
-        return (
-            <div data-tip data-for="ThemeSwitcher" className={styles.ThemeSwitcher}>
-                <Button 
-                    theme="icon" 
-                    onClick={() => theme === "dark" ? this.toggle("light") : this.toggle("dark") }>
-                    <PaletteIcon />
-                </Button>
-                <Tooltip place="left" id="ThemeSwitcher" message="Change Theme" />
-            </div>
-        );
-    }
-}
+const ThemeSwitcher = () => (
+    <div data-tip data-for="ThemeSwitcher" className={styles.ThemeSwitcher}>
+        <Button 
+            theme="icon" 
+            onClick={() => themeApi.changeTheme() }>
+            <Icon size="lg">
+                <PaletteIcon />
+            </Icon>
+        </Button>
+        <Tooltip place="left" id="ThemeSwitcher" message="Change Theme" />
+    </div>
+)
 
 export default ThemeSwitcher;
