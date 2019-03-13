d28a044f3eb9730df5c710c82ec3977d47a2bc41
Thanh Doan <tmdoan98@gmail.com>
2019-03-13 17:04:47 +0000

    add create list handler

--- client/src/api/listsApi.js
+++ client/src/api/listsApi.js
@@ -1,12 +1,16 @@
 import http from "./httpApi";
-import { LISTS_ENDPOINT } from "./endpoints";
+import { LISTS_ENDPOINT, CHANNELS_ENDPOINT } from "./endpoints";
 
+
+export function createList({ _id, name }) {
+    return http.post(`${CHANNELS_ENDPOINT}/${_id}/lists`, { name });
+}
 
 export function updateList({ listId, name }) {
     return http.put(`${LISTS_ENDPOINT}/${listId}`, { name });
 } 
 
-
 export default {
-    updateList
+    updateList,
+    createList
 }
