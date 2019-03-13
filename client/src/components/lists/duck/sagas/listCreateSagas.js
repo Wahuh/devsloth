84224ddae7cd34143368cae0c40f32daba764280
Thanh Doan <tmdoan98@gmail.com>
2019-03-13 17:05:21 +0000

    switch from channels api to lists api

--- client/src/components/lists/duck/sagas/listCreateSagas.js
+++ client/src/components/lists/duck/sagas/listCreateSagas.js
@@ -1,9 +1,9 @@
 import { call, put, takeEvery } from "redux-saga/effects";
 import { LIST_CREATE_REQUEST } from "../types";
-import { createListSuccess, createListFailure } from "../actions";
-import channelApi from "../../../../api/channelApi";
+import { createListFailure } from "../actions";
 import { addUiFetching, removeUiFetching } from "../../../ui/duck/actions";
 import { toastify } from "../../../ui/duck/sagas";
+import listsApi from "../../../../api/listsApi";
 
 
 export function* watchCreateListRequest() {
@@ -14,7 +14,7 @@ function* handleCreateList({ payload }) {
     yield put(addUiFetching("listCreate"));
 
     try {
-        yield call(channelApi.createList, payload);
+        yield call(listsApi.createList, payload);
         yield call(toastify, {
             message: "List created successfully!",
             duration: 3000,
