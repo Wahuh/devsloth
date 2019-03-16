d0ee94df6095578ab2e49d5fe14658ce15f72b62
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:51:27 +0000

    remove old imports

--- client/src/components/lists/ListDropdown/index.js
+++ client/src/components/lists/ListDropdown/index.js
@@ -1,14 +1,9 @@
 import { connect } from "react-redux";
 
 import React from "react";
-import Option from "../../reuse/Option";
-import Select from "../../reuse/Select";
-import Menu from "../../reuse/Menu";
-import MenuItem from "../../reuse/MenuItem";
+
 import { getSelectedChannelLists } from "../duck/selectors";
 import styles from "./ListDropdown.scss";
-import ListItem from "../../reuse/ListItem";
-import Button from "../../reuse/Button";
 import Dropdown from "../../reuse/Dropdown";
 import DropdownItem from "../../reuse/DropdownItem";
 
