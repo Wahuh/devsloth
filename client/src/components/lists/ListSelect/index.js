7c38e089eb69032419b37cd91e35cee818e51cd8
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:50:34 +0000

    add list select

---
+++ client/src/components/lists/ListSelect/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import Select from "../../reuse/Select";
+import styles from "./ListSelect.scss";
+import ListOption from "../duck/ListOption";
+
+const ListSelect = ({ listIds, onSelect, value }) => {
+    useEffect(() => {
+        onSelect(listIds[0]);
+    }, []);
+    const handleChange = ({ currentTarget }) => {
+        const { value } = currentTarget;
+        onSelect(value);
+    }
+    return (
+        <section className={styles.ListSelectBar}>
+            <Select onChange={handleChange} value={value}>
+                {listIds.map(id => <ListOption listId={id} />)}
+            </Select>
+        </section>
+    );
+}
+
+export default ListSelect;
---
+++ client/src/components/lists/duck/ListOption/index.js
@@ -0,0 +1,20 @@
+import { connect } from "react-redux";
+import React from "react";
+import { getListName } from "../selectors";
+import Typography from "../../../reuse/Typography";
+import Option from "../../../reuse/Option";
+
+const ListOption = ({ listId, name }) => {
+    console.log(name);
+    return (
+    <Option value={listId}>
+        {name}
+    </Option>
+)};
+
+
+const mapStateToProps = (state, ownProps) => ({
+    name: getListName(state, ownProps)
+});
+
+export default connect(mapStateToProps)(ListOption);
