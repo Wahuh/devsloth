4b657da2f5ab3ce70f75a2079026811f83c8c79a
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:54:28 +0000

    remove old event handlers

--- server/src/bin/socket.js
+++ server/src/bin/socket.js
@@ -29,45 +29,6 @@ module.exports = function(io) {
         socket.on("sendMessageTypingStart", memberId => socket.broadcast.emit("receiveMessageTypingStart", memberId));
         socket.on("sendMessageTypingStop", memberId => socket.broadcast.emit("receiveMessageTypingStop", memberId));
 
-        socket.on("sendGroupConnect", group => {
-            const { _id: groupId, channels: channelIds } = group;
-            socket.join(groupId);
-            for (let i = 0; i < channelIds.length; i++) {
-                socket.join(channelIds[i]);
-            }
-            socket.emit("receiveGroupConnect", group);
-        });
-
-
-        socket.on("sendMemberChannelJoin", member => emit(receiveMemberChannelJoin(member)));
-        socket.on("sendMemberChannelLeave", member => emit(receiveMemberChannelLeave(member)));
-        socket.on("sendMemberGroupJoin", member => socket.to(member.group).broadcast.emit("receiveMemberGroupJoin", member));
-        socket.on("sendMemberGroupLeave", member => emit(receiveMemberGroupLeave(member)));
-
-        socket.on("sendGroupCreate", () => {});
-        socket.on("sendGroupDelete", () => {});
-        socket.on("sendGroupUpdate", () => {});
-        socket.on("sendGroupMemberLeave", () => {});
-
-        socket.on("sendChannelConnect", channel => {
-            const { _id: channelId, group } = channel;
-            console.log("channelReceived");
-            socket.join(channelId);
-            socket.to(group).emit("receiveChannelConnect", channel);
-        });
-        socket.on("sendChannelCreate", channel => {
-            console.log("toCreate", channel);
-            socket.to(channel.group).emit("receiveChannelCreate", channel)
-            console.log(channel.group, io.sockets.adapter.rooms[channel.group].length);
-        }
-        );
-        socket.on("sendChannelDelete", () => {});
-        socket.on("sendChannelUpdate", () => {});
-        socket.on("sendChannelMemberJoin", () => {});
-        socket.on("sendChannelMemberLeave", () => {});
-        socket.on("sendChannelMemberTypingStart", () => {});
-        socket.on("sendChannelMemberTypingStop", () => {});
-
         socket.on("connectAll", data => {
             if (data) {
                 const groupIds = Object.keys(data);
@@ -86,54 +47,8 @@ module.exports = function(io) {
             }
         })
 
-        socket.on("groupCreate", ({ channelIds, groupId }) => {
-            socket.join(groupId);
-
-            for (let i = 0; i < channelIds.length; i++) {
-                socket.join(channelIds[i]);
-            }
-        });
-        socket.on("groupJoin", member => {
-            const { group: groupId, channels: channelIds } = member;
-            socket.to(groupId).emit("memberJoin", member);
-
-            socket.join(groupId);
-            for (let i = 0; i < channelIds.length; i++) {
-                socket.join(channelIds[i]);
-            }
-        });
-
-        socket.on("channel", payload => socket.join(Object.values(payload)[0]));
-        socket.on("channelCreate", ({ channelIds }) => {
-            for (let i = 0; i < channelIds.length; i++) {
-                socket.join(channelIds[i]);
-            }
-        });
-
-        socket.on("memberJoinedGroup", action => {
-            const { payload } = action;
-            const { entities, result: memberId} = payload
-            const { members } = entities;
-
-            const member = members[memberId];
-            const { group: groupId, channels: channelIds } = member;
-
-            console.log("MEMBERJOINED GROUP");
-            if (groups.hasOwnProperty(groupId)) {
-                if (!groups[groupId].includes(memberId)) {
-                    groups[groupId].push(memberId);
-                }
-            } else {
-                groups[groupId] = [memberId];
-            }
-            socket.join(groupId);
-            for (let i = 0; i < channelIds.length; i++) {
-                socket.join(channelIds[i]);
-            }
-            console.log("MEMBER", member);
-            console.log(groupId);
-            socket.to(groupId).emit("memberJoinedGroup", action);
-            //get id of new member, channelIds and groupIds
-        });
+        socket.on("channel task create", task => {
+            
+        })
     });
 }
