828c1298c78c3705f7f24adf489ae989d55d2a47
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:39:13 +0000

    convert to functional component + hooks

--- client/src/components/tasks/TaskList/VirtualList/index.js
+++ client/src/components/tasks/TaskList/VirtualList/index.js
@@ -1,72 +1,71 @@
-import React, { Component } from "react";
+import { connect } from "react-redux";
+import React, { useEffect, useRef } from "react";
 import { VariableSizeList as List } from 'react-window';
 import { SortableContainer, SortableElement } from "react-sortable-hoc";
 import styles from "./VirtualList.scss";
 import taskStyles from "../../Task/Task.scss";
 import Task from "../../Task";
 import isEqual from "lodash.isequal";
+import { makeGetListTaskIdsOrdered, makeGetListTaskNames, makeGetListTaskNamesOrdered } from "../../../lists/duck/selectors";
 
 const measure = document.getElementById("measure");
 
-const SortableItem = SortableElement(({ style, task }) => {
+const SortableItem = SortableElement(({ style, taskId }) => {
     return (
         <div style={style}>
-            <Task task={task} />
+            <Task taskId={taskId} />
         </div>
     );
 });
 
+const VirtualList = ({ taskIds, taskNames, height }) => {
+    const listRef = useRef(null);
+    useEffect(() => {
+        listRef.current.scrollToItem(taskIds.length - 1);
+    });
 
-class VirtualList extends Component {
-    listRef = React.createRef();
-
-    componentDidUpdate(prevProps) {
-        const { tasks } = this.props;
-
-        if (!isEqual(tasks, prevProps.tasks)) {
-            this.listRef.current.resetAfterIndex(0, true);
-            if (tasks.length != prevProps.tasks.length) {
-                this.listRef.current.scrollToItem(tasks.length - 1);
-            }
-        }
-    }
-
-    getItemSize = index => {
+    const getItemSize = index => {
         const text = document.createElement("p");
         text.classList.add(taskStyles.TextMock);
-        text.innerHTML = this.props.tasks[index].name;
+        text.innerHTML = taskNames[index];
         measure.appendChild(text);
         const height = text.clientHeight;
         measure.innerHTML = "";
         return height + 18;
     }
+    const Row = ({ index, style }) => (
+        <SortableItem index={index} style={style} taskId={taskIds[index]} />
+    );
 
-    Row = ({ index, style }) => {
-        const { tasks } = this.props;
-        return (
-            <SortableItem index={index} style={style} task={tasks[index]} />
-        );
-    };
+    return (
+        <List 
+            ref={listRef}
+            width={280}
+            height={height}
+            itemSize={getItemSize}
+            itemCount={taskIds.length}
+            className={styles.Scroller}
+        >
+            {Row}
+        </List>
+    );
+};
 
-    render() {
-        const { 
-            height,
-            tasks
-        } = this.props;
-        console.log("virutal", tasks);
-        return (
-            <List 
-                ref={this.listRef}
-                width={280}
-                height={height}
-                itemSize={this.getItemSize}
-                itemCount={tasks.length}
-                className={styles.Scroller}
-            >
-                {this.Row}
-            </List>
-        );
-    }
+const makeMapStateToProps = () => {
+    const getListTaskNamesOrdered = makeGetListTaskNamesOrdered();
+    const mapStateToProps = (state, ownProps) => ({
+        taskNames: getListTaskNamesOrdered(state, ownProps),
+    });
+    return mapStateToProps
 }
 
-export default SortableContainer(VirtualList);
+// if (!isEqual(tasks, prevProps.tasks)) {
+//     this.listRef.current.resetAfterIndex(0, true);
+//     if (tasks.length != prevProps.tasks.length) {
+//         this.listRef.current.scrollToItem(tasks.length - 1);
+//     }
+// }
+
+export default SortableContainer(
+    connect(makeMapStateToProps)(VirtualList)
+);
