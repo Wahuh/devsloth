4ca0329cd8694c44a601f8abc378e9c4e01cba83
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:43:06 +0000

    simplify saga

--- client/src/components/channel/duck/sagas/channelUpdateSagas.js
+++ client/src/components/channel/duck/sagas/channelUpdateSagas.js
@@ -1,29 +1,27 @@
 import channelApi from "../../../../api/channelApi";
-import { call, take, put } from "redux-saga/effects";
+import { call, takeEvery, put } from "redux-saga/effects";
 import { CHANNEL_UPDATE_REQUEST } from "../types";
 import { updateChannelSuccess, updateChannelFailure } from "../actions";
+import { toastify } from "../../../ui/duck/sagas";
+import { addUiFetching, removeUiFetching } from "../../../ui/duck/actions";
 
-export function* watchUpdateChannelRequest() {
-    while(true) {
-        const { payload } = yield take(CHANNEL_UPDATE_REQUEST);
-        yield call(handleUpdateChannel, payload);
-    }
+export function* watchChannelUpdateRequest() {
+    yield takeEvery(CHANNEL_UPDATE_REQUEST, handleChannelUpdate);
 }
 
-function* handleUpdateChannel(payload) {
+function* handleChannelUpdate({ payload }) {
+    yield put(addUiFetching("channelUpdate"));
     try {
-        yield call(updateChannel, payload);
+        yield call(channelApi.updateChannel, payload);
+        yield call(toastify, {
+            message: "Channel updated successfully!",
+            duration: 3000,
+            status: "success"
+        });
         // yield put(updateChannelSuccess(data));
     } catch (error) {
         yield put(updateChannelFailure(error));
-    }
-}
-
-function* updateChannel(payload) {
-    try {
-        const response = yield call(channelApi.updateChannel, payload);
-        return response;
-    } catch (error) {
-        throw(error);
+    } finally {
+        yield put(removeUiFetching("channelUpdate"));
     }
 }
