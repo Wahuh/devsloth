7b0a6985e8e5fc6fff155955a41c702cd8863f88
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:50:53 +0000

    change put action

--- client/src/components/group/duck/sagas/groupRemoveSagas.js
+++ client/src/components/group/duck/sagas/groupRemoveSagas.js
@@ -1,7 +1,7 @@
 import { put, takeEvery, select } from "redux-saga/effects";
 import { GROUP_DELETE_SUCCESS, GROUP_LEAVE_SUCCESS } from "../types";
 import { removeGroup } from "../actions";
-import { getDenormalizedGroup } from "../selectors";
+import { getGroupChannelIds } from "../selectors";
 import { removeChannels } from "../../../channel/duck/actions";
 
 export function* watchRemoveGroup() {
@@ -14,8 +14,6 @@ export function* watchRemoveGroup() {
 function* handleRemoveGroup({ payload }) {
     const { result: groupId } = payload;
 
-    const { taskIds, channelIds, memberIds } = yield select(getDenormalizedGroup, groupId);
-    yield put(removeGroup(payload));
-    console.log({ taskIds, channelIds, memberIds });
-    yield put(removeChannels({ result: channelIds }));
+    const channelIds = yield select(getGroupChannelIds, groupId);
+    yield put(removeGroup({ ...payload, channelIds }));
 }
