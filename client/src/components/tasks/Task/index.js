b17267bd5086ed8d9ebf12603d62b12243e6614b
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:45:27 +0000

    add withRouter and dropdown component

--- client/src/components/tasks/Task/index.js
+++ client/src/components/tasks/Task/index.js
@@ -1,7 +1,8 @@
 import { connect } from "react-redux";
 import { addUiModal } from "../../ui/duck/actions";
 
-import React, { Fragment } from "react";
+import React, { useState, useRef } from "react";
+import { NavLink, withRouter } from "react-router-dom";
 import BulletListIcon from "../../reuse/icons/BulletListIcon";
 import Button from "../../reuse/Button";
 import ChatBubbleIcon from "../../reuse/icons/ChatBubbleIcon";
@@ -12,34 +13,58 @@ import TextArea from "../../reuse/TextArea";
 import Typography from "../../reuse/Typography";
 import ListItem from "../../reuse/ListItem";
 import styles from "./Task.scss";
-import { MODAL_TASK_EDIT } from "../../ui/constants";
+import { MODAL_TASK_EDIT, MODAL_TASK_DROPDOWN } from "../../ui/constants";
 import Icon from "../../reuse/Icon"
 
 import { selectTask } from "../duck/actions";
+import TaskDropdown from "../TaskDropdown";
+import { getTask } from "../duck/selectors";
 
-export const Task = ({ task, onEdit, onSelect }) => {
+export const Task = ({ task, onEdit, onSelect, match }) => {
+    const taskRef = useRef(null);
+    const [showDropdown, setShowDropdown] = useState(false);
+    
+    const [position, setPosition] = useState(null);
     const { _id } = task;
-    const onClick = () => {
+
+    const onDropdown = () => {
+        onSelect(_id);
+        const clientRect = taskRef.current.getBoundingClientRect()
+        setPosition({ top: clientRect.top + document.documentElement.scrollTop, left: clientRect.left }  );
+        setShowDropdown(true);
+    }
+
+    const onClick = event => {
+        event.preventDefault();
         onSelect(_id);
         onEdit();
     };
 
     return (
-        <ListItem className={styles.Task} onClick={onClick}>
-            <Typography color="primary">
-                {task.name}
-            </Typography>
-
-            <Button className={styles.TaskEditButton}>
+        <ul ref={taskRef} className={styles.Task}>
+            <NavLink to={`${match.url}/${_id}`} onClick={onClick}>
+                <Typography color="primary">
+                    {task.name}
+                </Typography>
+            </NavLink>
+            <Button onClick={onDropdown} type="button" className={styles.TaskEditButton}>
                 <Icon size="md">
                     <EditIcon />
                 </Icon>
             </Button>
-        </ListItem>
+            {showDropdown && <TaskDropdown onHide={() => setShowDropdown(false)} position={position} />}
+        </ul>
     )
 }
 
-export default connect(null, {
-    onEdit: () => addUiModal(MODAL_TASK_EDIT),
-    onSelect: selectTask
-})(Task);
+const mapStateToProps = (state, ownProps) => ({
+    task: getTask(state, ownProps.taskId)
+});
+
+export default withRouter(
+    connect(mapStateToProps, {
+        onEdit: () => addUiModal(MODAL_TASK_EDIT),
+        onSelect: selectTask,
+        onShowModal: () => addUiModal(MODAL_TASK_DROPDOWN)
+    })(Task)
+);
