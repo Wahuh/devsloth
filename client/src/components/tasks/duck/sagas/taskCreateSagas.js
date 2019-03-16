e0544742f39a33cb74ca3627f83d93823e6687b8
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:54:13 +0000

    change to while and take

--- client/src/components/tasks/duck/sagas/taskCreateSagas.js
+++ client/src/components/tasks/duck/sagas/taskCreateSagas.js
@@ -1,21 +1,21 @@
-import { call, takeEvery, put } from "redux-saga/effects";
+import { call, takeEvery, take, put } from "redux-saga/effects";
 import tasksApi from "../../../../api/tasksApi";
 import { createTaskFailure } from "../actions";
 import { TASK_CREATE_REQUEST, TASK_CREATE_SUCCESS } from "../types";
 import { toastify } from "../../../ui/duck/sagas";
 
 export function* watchCreateTaskRequest() {
-    yield takeEvery(TASK_CREATE_REQUEST, handleCreateTask);
+    while(true) {
+        const { payload } = yield take(TASK_CREATE_REQUEST);
+        yield call(handleCreateTask, payload);
+    }
+    // yield takeEvery(TASK_CREATE_REQUEST, handleCreateTask);
 }
 
-function* handleCreateTask({ payload }) {
+function* handleCreateTask(payload) {
     try {
+        console.log("request", payload);
         yield call(tasksApi.createTask, payload);
-        yield call(toastify, {
-            message: "Task created successfully!",
-            duration: 1500,
-            status: "success"
-        });
     } catch(error) {
         yield put(createTaskFailure(error));
     }
