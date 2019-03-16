01aae4aabe8e985b35986d3d3353d64e4f06a123
Thanh Doan <tmdoan98@gmail.com>
2019-03-16 11:42:10 +0000

    simplify saga

--- client/src/components/channel/duck/sagas/channelDeleteSagas.js
+++ client/src/components/channel/duck/sagas/channelDeleteSagas.js
@@ -1,30 +1,22 @@
 import channelApi from "../../../../api/channelApi";
-import { call, take, put } from "redux-saga/effects";
-import { removeUiModal } from "../../../ui/duck/actions";
-import { CHANNEL_DELETE_REQUEST, CHANNEL_DELETE_SUCCESS } from "../types";
-import { deleteChannelSuccess, deleteChannelFailure, deleteChannel } from "../actions";
+import { call, takeEvery, put } from "redux-saga/effects";
+import { removeUiModal, addUiFetching, removeUiFetching } from "../../../ui/duck/actions";
+import { CHANNEL_DELETE_REQUEST } from "../types";
+import { deleteChannelFailure } from "../actions";
 import { MODAL_CHANNEL_SETTINGS } from "../../../ui/constants";
 
-export function* watchDeleteChannelRequest() {
-    while(true) {
-        const { payload } = yield take(CHANNEL_DELETE_REQUEST);
-        yield call(handleDeleteChannel, payload);
-    }
+export function* watchChannelDeleteRequest() {
+    yield takeEvery(CHANNEL_DELETE_REQUEST, handleChannelDelete);
 }
 
-function* handleDeleteChannel(payload) {
+function* handleChannelDelete({ payload }) {
+    yield put(addUiFetching("channelDelete"));
     try {
-        const { data } = yield call(channelApi.deleteChannel, payload);
-        yield put(deleteChannelSuccess(data))
+        yield call(channelApi.deleteChannel, payload);
         yield put(removeUiModal(MODAL_CHANNEL_SETTINGS));
     } catch (error) {
         yield put(deleteChannelFailure(error));
+    } finally {
+        yield put(removeUiFetching("channelDelete"));
     }
-}
-
-export function* watchDeleteChannelSuccess() {
-    //delete all tasks and messages
-    const { payload } = yield take(CHANNEL_DELETE_SUCCESS);
-    console.log("CHAN", payload);
-    // yield put(deleteChannel(payload));
 }
