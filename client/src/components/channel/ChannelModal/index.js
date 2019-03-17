4df7a4e1266f9fa6b894e6805203a3fbc9f8c479
Thanh Doan <tmdoan98@gmail.com>
2019-03-17 17:49:15 +0000

    change modal size

--- client/src/components/channel/ChannelModal/index.js
+++ client/src/components/channel/ChannelModal/index.js
@@ -6,11 +6,10 @@ import ChannelCreateForm from "../../channel/ChannelCreateForm";
 
 import CloseButton from "../../reuse/buttons/CloseButton";
 import Modal from "../../reuse/Modal";
-import styles from "./ChannelModal.scss";
 import { MODAL_CHANNEL_CREATE } from "../../ui/constants";
 
 const ChannelModal = ({ onHide }) => (
-    <Modal onHide={onHide}>
+    <Modal size="md" onHide={onHide}>
         <CloseButton onClick={onHide} />
         <ChannelCreateForm />
     </Modal>
