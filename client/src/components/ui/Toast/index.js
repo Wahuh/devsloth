771a7f373f798d6e49549ab2612ff7ebd85e6abd
Thanh Doan <tmdoan98@gmail.com>
2019-03-17 17:55:44 +0000

    convert to functional component + hooks

--- client/src/components/ui/Toast/index.js
+++ client/src/components/ui/Toast/index.js
@@ -1,11 +1,9 @@
 import { connect } from "react-redux";
 
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Typography from "../../reuse/Typography";
-import SuccessIcon from "../../reuse/icons/SuccessIcon";
 import styles from "./Toast.scss";
 import { removeUiToast } from "../duck/actions";
-import CloseButton from "../../reuse/buttons/CloseButton";
 import Button from "../../reuse/Button";
 import CloseIcon from "../../reuse/icons/CloseIcon";
 
@@ -14,35 +12,30 @@ const statuses = {
     "error": styles.Failure,
 }
 
-class Toast extends Component {
-    componentDidMount() {
-        const { toast, onClose } = this.props;
-        const { _id, duration } = toast;
+const Toast = ({ toast, onClose }) => {
+    const { _id, duration, status, message } = toast;
+    useEffect(() => {
         if (duration) {
             setTimeout(() => onClose(_id), 2000);
         }
-    }
+    }, []);
 
-    render() {
-        const { toast, onClose } = this.props;
-        const { _id, message, status, duration } = toast;
-        return (
-            <div className={styles.Toast}>
-                <div className={`${styles.Status} ${statuses[status]}`}>
-                </div>
-                <div className={styles.Message}>
-                    <Typography color="secondary">
-                        {message}
-                    </Typography>
-                </div>
-                {!duration && 
+    return (
+        <div className={styles.Toast}>
+            <div className={`${styles.Status} ${statuses[status]}`}>
+            </div>
+            <div className={styles.Message}>
+                <Typography color="secondary">
+                    {message}
+                </Typography>
+            </div>
+            {!duration && 
                 <Button theme="icon" onClick={() => onClose(_id)}>
                     <CloseIcon />
                 </Button>
-                }
-            </div>
-        );
-    }
+            }
+        </div>
+    );
 }
 
 export default connect(null, {
