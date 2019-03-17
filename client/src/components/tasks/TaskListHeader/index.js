4c86ddf7d5eea9afbc039a6be4262cea48776bb4
Thanh Doan <tmdoan98@gmail.com>
2019-03-17 17:56:19 +0000

    convert to functional component + hooks

--- client/src/components/tasks/TaskListHeader/index.js
+++ client/src/components/tasks/TaskListHeader/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState } from "react";
 import { connect } from "react-redux";
 import AddButton from "../../reuse/buttons/AddButton";
 import Column from "../../reuse/Column";
@@ -14,119 +14,96 @@ import Row from "../../reuse/Row";
 import ListDropdown from "../../lists/ListDropdown";
 import { updateListRequest } from "../../lists/duck/actions";
 
-class TaskListHeader extends Component {
-    state = {
-        isRenaming: false,
-        isDropdownShown: false,
-        list: {
-            name: ""
-        }
+const TaskListHeader = ({ list }) => {
+    const [ isRenaming, setIsRenaming ] = useState(false);
+    const [ name, setName ] = useState("");
+    const [ isDropdownVisible, setIsDropdownVisible ] = useState(false);
+
+    const handleRename = () => {
+        setName(list.name);
+        setIsRenaming(true);
+        setIsDropdownVisible(!isDropdownVisible);
+    };
+
+    const handleDropdown = () => {
+        setIsDropdownVisible(!isDropdownVisible);
     }
 
-    handleRename = () => {
-        const { list } = this.props;
-        const { name } = list;
-        this.setState(prevProps => ({ 
-            isRenaming: true,
-            isDropdownShown: !prevProps.isDropdownShown,
-            list: { name }
-        }));
+    const handleChange = ({ currentTarget }) => {
+        const { value } = currentTarget;
+        setName(value);
     }
-
-    handleDropdown = () => {
-        this.setState(prevProps => ({ isDropdownShown: !prevProps.isDropdownShown }));
-    }
-
-    handleChange = ({ currentTarget }) => {
-        const { name, value } = currentTarget;
-
-        const list = { ...this.state.list };
-        list[name] = value;
-        this.setState({ list });
-    }
-
-    handleSubmit = () => {
-        const { list } = this.state;
+    const handleSubmit = () => {
         const { onEdit } = this.props;
-        if (list.name && list.name !== this.props.list.name) {
-            onEdit({ ...list, listId: this.props.list._id });
+        if (name && name !== list.name) {
+            onEdit({ name, listId: list._id });
         }
-        this.setState({
-            isRenaming: false
-        });
+        setIsRenaming(false);
     }
 
-    render() {
-        const { list } = this.props;
-        const { isDropdownShown, isRenaming } = this.state;
-        let count;
-        if (list && list.tasks) {
-            if (list.tasks.length > 0) {
-                count = <div className={styles.TaskCount}>
-                            <Typography type="caption" color="quaternary">
-                                {list.tasks.length}
-                            </Typography>
-                        </div>
-            }
-
-        } else {
-            count = null;
-        }
-
-        return (
-            <section className={styles.TaskListHeader}>
-                {isRenaming ? (
-                    <Input
-                        autoFocus
-                        className={styles.TaskListHeaderInput}
-                        type="text"
-                        value={this.state.list.name}
-                        onBlur={this.handleSubmit}
-                        onEnter={this.handleSubmit}
-                        onChange={this.handleChange}
-                        name="name"
-                    />
-                ) : (
-                    <Fragment>
-                        <Typography type="subheading" color="secondary">
-                            {list.name}
+    return (
+        <section className={styles.TaskListHeader}>
+            {isRenaming ? (
+                <Input
+                    autoFocus
+                    className={styles.TaskListHeaderInput}
+                    type="text"
+                    value={name}
+                    onBlur={handleSubmit}
+                    onEnter={handleSubmit}
+                    onChange={handleChange}
+                    name="name"
+                />
+            ) : (
+                <Fragment>
+                    <Typography type="subheading" color="secondary">
+                        {list.name}
+                    </Typography>
+                        
+                    {list.tasks && (list.tasks.length > 0 && 
+                    <div className={styles.TaskCount}>
+                        <Typography type="caption" color="quaternary">
+                            {list.tasks.length}
                         </Typography>
-                            
-                        {count}
-                    </Fragment>
-                )}
-                
-                <div className={styles.Wrapper} data-tip data-for="ListOptions">
-                    {!isRenaming && 
-                        <Button onClick={this.handleDropdown} theme="icon">
-                            <SearchIcon />
-                        </Button>
+                    </div>) 
                     }
+                </Fragment>
+            )}
+            
+            <div className={styles.Wrapper} data-tip data-for="ListOptions">
+                {!isRenaming && 
+                    <Button onClick={handleDropdown} theme="icon">
+                        <SearchIcon />
+                    </Button>
+                }
 
-                    <div className={styles.RelativeWrapper}>
-                        <Button onClick={this.handleDropdown} theme="icon">
-                            <MoreOptionsIcon />
-                        </Button>
+                <div className={styles.RelativeWrapper}>
+                    <Button onClick={handleDropdown} theme="icon">
+                        <MoreOptionsIcon />
+                    </Button>
 
-                        {isDropdownShown && 
-                        <ListDropdown 
-                            onRename={this.handleRename}
-                            onClose={this.handleDropdown}
-                        />
-                    }
-                    </div>
-  
-                    {/* <Tooltip id="ListOptions" message="List Options" /> */}
+                    {isDropdownVisible && 
+                    <ListDropdown 
+                        onRename={handleRename}
+                        onClose={handleDropdown}
+                    />
+                }
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
 }
 
-const mapStateToProps = (state, ownProps) => ({
-    list: getList(state, ownProps)
-});
+const makeMapStateToProps = (initialState, initialProps) => {
+    const { listId } = initialProps;
+    const mapStateToProps = state => {
+        const { lists } = state;
+        const list = lists.byId[listId];
+        return ({ list });
+    }
+    return mapStateToProps;
+}
 
-export default connect(mapStateToProps, {
+export default connect(makeMapStateToProps, {
     onEdit: updateListRequest
 })(TaskListHeader);
