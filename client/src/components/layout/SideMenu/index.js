69884f34ceafda0d518ff006761c83417249dae4
Thanh Doan <tmdoan98@gmail.com>
2019-03-17 17:51:39 +0000

    convert to functional component + hooks

--- client/src/components/layout/SideMenu/index.js
+++ client/src/components/layout/SideMenu/index.js
@@ -1,33 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styles from "./SideMenu.scss";
-
-class SideMenu extends Component {
-    state = {
-        show: false
-    }
-
-    hide = () => {
+ 
+const SideMenu = ({ children }) => {
+    const [ isVisible, setIsVisible ] = useState(false);
+    const handleHide = () => {
         document.getElementById(styles.SideMenu).style.transform = "translate3d(-80vw, 0, 0)";
         document.getElementById(styles.SideMenuOverlay).style.visibility = "hidden";
         document.getElementById(styles.SideMenuContainer).style.visibility = "hidden";
     }
-
-    toggleMenu = () => {
-        this.setState({
-            show: !this.state.visible
-        });
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible)
     }
-
-    render() {
-        return (
-            <div id={styles.SideMenuContainer}>
-                <div id={styles.SideMenu}>
-                    {this.props.children}
-                </div>
-                <div id={styles.SideMenuOverlay} onClick={this.hide}></div>
+    return (
+        <div id={styles.SideMenuContainer}>
+            <div id={styles.SideMenu}>
+                {children}
             </div>
-        );
-    }
+            <div id={styles.SideMenuOverlay} onClick={handleHide}></div>
+        </div>
+    );
 }
+
 
 export default SideMenu;
