12a83c485e8b8b4aad1468a1e19c921059eb21f3
Thanh Doan <tmdoan98@gmail.com>
2019-03-24 19:47:21 +0000

    add conditional for user created tasks

--- client/src/api/tasksApi.js
+++ client/src/api/tasksApi.js
@@ -1,7 +1,13 @@
 import http from "./httpApi";
-import { TASKS_ENDPOINT, LISTS_ENDPOINT } from "./endpoints";
+import { TASKS_ENDPOINT, LISTS_ENDPOINT, CURRENT_USER_ENDPOINT } from "./endpoints";
 
-export const createTask = task => http.post(`${LISTS_ENDPOINT}/${task.list}/tasks`, task);
+export const createTask = task => {
+    if (task.channel) {
+        return http.post(`${LISTS_ENDPOINT}/${task.list}/tasks`, task);
+    } else {
+        return http.post(`${CURRENT_USER_ENDPOINT}/lists/${task.list}/tasks`, task);
+    }
+}
 export const updateTask = task => http.put(`${TASKS_ENDPOINT}/${task._id}`, task);
 export const deleteTask = task => http.delete(`${TASKS_ENDPOINT}/${task._id}`)
 
@@ -9,5 +15,4 @@ export default {
     createTask,
     deleteTask,
     updateTask,
-
 }
