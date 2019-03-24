34c8384c6b03baae9a77175eb0984a465e354e20
Thanh Doan <tmdoan98@gmail.com>
2019-03-24 19:47:03 +0000

    add conditional for user created lists

--- client/src/api/listsApi.js
+++ client/src/api/listsApi.js
@@ -1,14 +1,19 @@
 import http from "./httpApi";
-import { LISTS_ENDPOINT, CHANNELS_ENDPOINT } from "./endpoints";
+import { LISTS_ENDPOINT, CHANNELS_ENDPOINT, CURRENT_USER_ENDPOINT } from "./endpoints";
 
 
 export function createList({ _id, name }) {
-    return http.post(`${CHANNELS_ENDPOINT}/${_id}/lists`, { name });
+    console.log("IDNAME", _id, name);
+    if (_id) {
+        return http.post(`${CHANNELS_ENDPOINT}/${_id}/lists`, { name });
+    } else {
+        return http.post(`${CURRENT_USER_ENDPOINT}/lists`, { name });
+    }
 }
 
 export function updateList({ listId, name }) {
     return http.put(`${LISTS_ENDPOINT}/${listId}`, { name });
-} 
+}
 
 export default {
     updateList,
