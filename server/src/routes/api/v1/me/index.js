358f52177436cc399ff265411cb4626f9d7a37b1
Thanh Doan <tmdoan98@gmail.com>
2019-03-24 19:49:05 +0000

    add route handler for user tasks and lists

--- server/src/routes/api/v1/me/index.js
+++ server/src/routes/api/v1/me/index.js
@@ -9,9 +9,13 @@ const {
     removeUser,
     updateUser
 } = require("../../../../controllers/user.controller");
+const { createList } = require("../../../../controllers/list.controller");
+const { createUserTask } = require("../../../../controllers/task.controller");
 
 me.get("/", auth, wrapAsync(getCurrentUser));
 me.delete("/", auth, wrapAsync(removeUser));
 me.put("/", auth, wrapAsync(updateUser));
+me.post("/lists", auth, wrapAsync(createList))
+me.post("/lists/:listId/tasks", auth, wrapAsync(createUserTask));
 
 module.exports = me;
