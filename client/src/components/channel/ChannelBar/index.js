c7f3babc1f712a9803995f6fbdd07fba689ffce5
Thanh Doan <tmdoan98@gmail.com>
2019-03-25 21:11:57 +0000

    add divider

--- client/src/components/channel/ChannelBar/index.js
+++ client/src/components/channel/ChannelBar/index.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import ChatIcon from "../../reuse/icons/ChatIcon";
-import TaskIcon from "../../reuse/icons/TaskIcon";
-import ChannelName from "../ChannelName";
-import Column from "../../reuse/Column";
-import ChannelViewSwitcher from "../ChannelViewSwitcher";
 import ChannelMemberCount from "../ChannelMemberCount";
-import Divider from "../../reuse/Divider";
-import Row from "../../reuse/Row";
 import ChannelTopic from "../ChannelTopic";
 import styles from "./ChannelBar.scss";
 
 const ChannelBar = () => (
     <div className={styles.ChannelBar}>
         <ChannelMemberCount />
-        <Divider vertical />
+        <hr className={styles.Divider}></hr>
         <ChannelTopic />
     </div>
 );
