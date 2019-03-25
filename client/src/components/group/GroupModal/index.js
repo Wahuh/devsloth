6d9b4d55f0314afb757c4196051ad01ad6015652
Thanh Doan <tmdoan98@gmail.com>
2019-03-25 21:16:41 +0000

    convert to functional component + hooks

--- client/src/components/group/GroupModal/index.js
+++ client/src/components/group/GroupModal/index.js
@@ -1,40 +1,34 @@
 import { connect } from "react-redux";
 import { removeUiModal } from "../../ui/duck/actions";
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import CloseButton from "../../reuse/buttons/CloseButton";
-import CreateOrJoin from "../CreateOrJoin";
+import GroupCreateOrJoin from "../GroupCreateOrJoin";
 import GroupCreateForm from "../GroupCreateForm";
 import GroupJoinForm from "../GroupJoinForm";
 import Modal from "../../reuse/Modal";
-import styles from "./GroupModal.scss";
 import { MODAL_GROUP_CREATE_OR_JOIN } from "../../ui/constants";
+ 
 
-class GroupModal extends Component {
-    state = {
-        screen: "createOrJoin",
+
+const GroupModal = ({ onHide }) => {
+    const [ screen, setScreen ] = useState("createOrJoin");
+
+    const screens = {
+        createOrJoin: <GroupCreateOrJoin
+                        onCreate={() => setScreen("create")} 
+                        onJoin={() => setScreen("join")} 
+                        />,
+        create: <GroupCreateForm onBack={() => setScreen("createOrJoin")} />,
+        join: <GroupJoinForm onBack={() => setScreen("createOrJoin")} />
     }
 
-    screens = {
-        createOrJoin: <CreateOrJoin onCreate={() => this.changeScreen("create")} onJoin={() => this.changeScreen("join")} />,
-        create: <GroupCreateForm onBack={() => this.changeScreen("createOrJoin")} />,
-        join: <GroupJoinForm onBack={() => this.changeScreen("createOrJoin")} />
-    }
-
-    changeScreen(screen) {
-        this.setState({ screen });
-    }
-
-    render() {
-        const { onHide } = this.props;
-        const { screen } = this.state;
-        return (
-            <Modal onHide={onHide}>
-                <CloseButton onClick={onHide} />
-                {this.screens[screen]}
-            </Modal>
-        );
-    }
+    return (
+        <Modal size="md" onHide={onHide}>
+            <CloseButton onClick={onHide} />
+            {screens[screen]}
+        </Modal>
+    );
 }
 
 export default connect(null, {
