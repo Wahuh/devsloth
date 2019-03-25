275985e8dc29b2a6a73acd9cb44ae943e3c308e5
Thanh Doan <tmdoan98@gmail.com>
2019-03-25 21:12:48 +0000

    add channel id redux state

--- client/src/components/channel/ChannelMenu/index.js
+++ client/src/components/channel/ChannelMenu/index.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ChannelMenu.scss";
 import Icon from "../../reuse/Icon";
 import TaskIcon from "../../reuse/icons/TaskIcon";
@@ -10,9 +10,18 @@ import TaskCreateForm from "../../tasks/TaskCreateForm";
 import ListSelect from "../../lists/ListSelect";
 import ChannelMenuList from "../ChannelMenuList";
 import MemberIcon from "../../reuse/icons/MemberIcon";
+import DragDropProvider from "../../dnd/DragDropProvider";
+import { getSelectedChannelId } from "../duck/selectors";
 
-const ChannelMenu = ({ listIds }) => {
+const ChannelMenu = ({ listIds, channelId }) => {
     const [ listId, setListId ] = useState("");
+    useEffect(() => {
+        if (listIds && listIds.length > 0) {
+            setListId(listIds[0]);
+        } else {
+            setListId("");
+        }
+    }, [ listIds ]);
     return (
         <section className={styles.ChannelMenu}>
             <ul className={styles.MenuBar}>
@@ -28,23 +37,25 @@ const ChannelMenu = ({ listIds }) => {
                     </Icon>
                 </li>
             </ul>
+            
 
-            <section className={styles.ChannelMenuView}>
-                {listIds && <ListSelect onSelect={setListId} value={listId} listIds={listIds} />}
-                {listId && 
-                    <ChannelMenuList>
-                        <TaskListHeader listId={listId} />
-                        <TaskList listId={listId} />
-                        <TaskCreateForm _id={listId} /> 
-                    </ChannelMenuList>
-                }
-            </section>
+                <section className={styles.ChannelMenuView}>
+                    {listIds && <ListSelect onSelect={setListId} value={listId} listIds={listIds} />}
+                    {listId && 
+                        <ChannelMenuList>
+                            <TaskListHeader listId={listId} />
+                            <TaskList listId={listId} />
+                            <TaskCreateForm channelId={channelId} listId={listId} /> 
+                        </ChannelMenuList>
+                    }
+                </section>
         </section>  
     );
 }
-
+//            <DragDropProvider>
 const mapStateToProps = state => ({
-    listIds: getChannelListIds(state)
+    listIds: getChannelListIds(state),
+    channelId: getSelectedChannelId(state)
 });
 
 export default connect(mapStateToProps)(ChannelMenu);
