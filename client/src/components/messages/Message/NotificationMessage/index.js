49d3a82bd8a38250ef9e7fee78a7e96c0a4a3a66
Thanh Doan <tmdoan98@gmail.com>
2019-03-25 21:17:42 +0000

    change children prop to text

--- client/src/components/messages/Message/NotificationMessage/index.js
+++ client/src/components/messages/Message/NotificationMessage/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import Typography from "../../../reuse/Typography";
 import styles from "./NotificationMessage.scss";
 
-const NotificationMessage = ({ children }) => (
+const NotificationMessage = ({ text }) => (
     <div className={styles.NotificationMessage}>
         <Typography color="primary" type="body">
-            {children}
+            {text}
         </Typography>
     </div>
 );
