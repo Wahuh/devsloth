972929c59c3ed303ac8f169c1ff1a6f25afb3847
Thanh Doan <tmdoan98@gmail.com>
2019-03-25 21:14:11 +0000

    add position relative wrapper

--- client/src/components/channel/ChannelView/index.js
+++ client/src/components/channel/ChannelView/index.js
@@ -2,12 +2,12 @@ import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
 import MainPanel from "../../layout/MainPanel";
 import SecondaryPanel from "../../layout/SecondaryPanel";
-import TaskBoard from "../../tasks/TaskBoard";
 import Row from "../../reuse/Row";
 import MessageList from "../../messages/MessageList";
-import MessageComposer from "../../chat/MessageComposer";
+import MessageComposer from "../../messages/MessageComposer";
 import ChannelMenu from "../ChannelMenu";
 import ChannelTaskBoard from "../ChannelTaskBoard";
+import MembersTyping from "../../members/MembersTyping";
 
 const ChannelView = ({ match }) => (
     <Row maxHeight maxWidth>
@@ -15,7 +15,10 @@ const ChannelView = ({ match }) => (
             <Fragment>
                 <MainPanel>
                     <MessageList />
-                    <MessageComposer />
+                    <div style={{ position: "relative", flexShrink: 0, width: "100%" }}>
+                        <MessageComposer />
+                        <MembersTyping />
+                    </div>
                 </MainPanel>
 
                 <SecondaryPanel>
