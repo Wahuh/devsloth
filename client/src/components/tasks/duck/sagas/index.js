93c6fe01ce329885431b99cd2159aecdd00f0504
Thanh Doan <tmdoan98@gmail.com>
2019-03-27 11:22:27 +0000

    clean up

--- client/src/components/tasks/duck/sagas/index.js
+++ client/src/components/tasks/duck/sagas/index.js
@@ -1,67 +1,20 @@
-import { all, call, take, put } from "redux-saga/effects";
-import tasksApi from "../../../../api/tasksApi";
-
-import { 
-    TASK_CREATE_REQUEST,
-    TASK_UPDATE_REQUEST,
-    TASK_DELETE_REQUEST,
-    TASK_LIST_CREATE_REQUEST
-} from "../types";
-import { 
-    createTaskSuccess, 
-    createTaskFailure,
-    updateTaskSuccess,
-    updateTaskFailure,
-    createTaskListSuccess,
-    createTaskListFailure,
-    createLocalTask,
-    updateLocalTask,
-} from "../actions";
-
-import { removeUiModal } from "../../../ui/duck/actions";
-import { MODAL_TASK_EDIT } from "../../../ui/constants";
+import { all } from "redux-saga/effects";
 import { watchTaskAdd } from "./taskAddSagas";
-import { watchCreateTaskRequest } from "./taskCreateSagas";
+import { watchTaskCreateRequest } from "./taskCreateSagas";
+import { watchTaskDeleteRequest } from "./taskDeleteSagas";
 import { watchTaskEdit } from "./taskEditSagas";
-import { watchTaskUpdateRequest } from "./taskUpdateSagas";
 import { watchTaskMoveRequest } from "./taskMoveSagas";
-
-
-
-
-export function* watchDeleteTaskRequest() {
-    while(true) {
-        const { payload } = yield take(TASK_DELETE_REQUEST);
-        yield call(handleDeleteTask, payload);
-    }
-}
-
-function* handleDeleteTask(payload) {
-    try {
-        const { data } = yield call(deleteTask, payload);
-        yield put(deleteTaskSuccess(data));
-        yield put(removeUiModal(MODAL_TASK_EDIT));
-    } catch(error) {
-        yield put(deleteTaskFailure(error));
-    }
-}
-
-function* deleteTask(payload) {
-    try {
-        const response = yield call(tasksApi.deleteTask, payload);
-        return response;
-    } catch (error) {
-        throw(error);
-    }
-}
+import { watchTaskRemove } from "./taskRemoveSagas";
+import { watchTaskUpdateRequest } from "./taskUpdateSagas";
 
 export default function* tasksSaga() {
     yield all([
-        watchCreateTaskRequest(),
-        watchTaskUpdateRequest(),
-        watchTaskMoveRequest(),
         watchTaskAdd(),
+        watchTaskCreateRequest(),
+        watchTaskDeleteRequest(),
         watchTaskEdit(),
-
+        watchTaskMoveRequest(),
+        watchTaskRemove(),
+        watchTaskUpdateRequest(),
     ]);
 }
