e1ba20c65ec8667ea4d18f58e31053e58abafee5
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:51:48 +0100

    add transitions

--- client/src/components/layout/SideMenu/index.js
+++ client/src/components/layout/SideMenu/index.js
@@ -1,25 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useRef, Fragment } from "react";
+import { connect} from "react-redux";
+import { CSSTransition } from 'react-transition-group';
 import styles from "./SideMenu.scss";
  
+
+
 const SideMenu = ({ children }) => {
     const [ isVisible, setIsVisible ] = useState(false);
+    const overlayRef = useRef(null);
     const handleHide = () => {
-        document.getElementById(styles.SideMenu).style.transform = "translate3d(-80vw, 0, 0)";
-        document.getElementById(styles.SideMenuOverlay).style.visibility = "hidden";
-        document.getElementById(styles.SideMenuContainer).style.visibility = "hidden";
+        const sideMenu = document.getElementById(styles.SideMenu);
+        const sideMenuOverlay = document.getElementById(styles.SideMenuOverlay);
+        sideMenu.style.transform = "translate3d(-80vw, 0, 0)";
+        sideMenuOverlay.style.opacity = 0;
+        setTimeout(function() {
+            sideMenuOverlay.style.visibility = "hidden";
+        }, 400);
     }
     const toggleVisibility = () => {
         setIsVisible(!isVisible)
     }
     return (
-        <div id={styles.SideMenuContainer}>
+        // <div id={styles.SideMenuContainer}>
+        <Fragment>
             <div id={styles.SideMenu}>
                 {children}
             </div>
-            <div id={styles.SideMenuOverlay} onClick={handleHide}></div>
-        </div>
+
+            {/* <CSSTransition
+                in={isVisible}
+                timeout={300}
+                unmountOnExit
+                classNames={{
+                    leave: styles.Leave,
+                    leaveActive: styles.LeaveActive,
+                    appear: styles.Appear,
+                    appearActive: styles.AppearActive
+                }}
+            > */}
+                <div ref={overlayRef} id={styles.SideMenuOverlay} onClick={handleHide}></div>
+            {/* </CSSTransition> */}
+        </Fragment>
+        // </div>
     );
 }
-
 
 export default SideMenu;
