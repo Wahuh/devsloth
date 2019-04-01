ee8d02005cacefe535c1b5d8dd01a5af7ef63b42
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:43:24 +0100

    update CSSTransition group

--- client/src/components/ui/ToastRoot/index.js
+++ client/src/components/ui/ToastRoot/index.js
@@ -1,32 +1,32 @@
 import { connect } from "react-redux";
 
 import React from "react";
-import { CSSTransitionGroup } from 'react-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Toast from "../Toast";
 import { getAllToasts } from "../duck/selectors";
 import styles from "./ToastRoot.scss";
 
 const ToastRoot = ({ toasts }) => (
-    <CSSTransitionGroup
-        component="section"
-        transitionName={{
-            enter: styles.Appear,
-            enterActive: styles.AppearActive,
-            leave: styles.Leave,
-            leaveActive: styles.LeaveActive,
-            appear: styles.Appear,
-            appearActive: styles.AppearActive
-        }} 
-        transitionAppear={true} 
-        transitionAppearTimeout={300} 
-        transitionEnter={true}
-        transitionEnterTimeout={300} 
-        transitionLeave={true} 
-        transitionLeaveTimeout={300}>
+    <TransitionGroup component={null}>
         {
-            toasts.map(toast => <Toast key={toast._id} toast={toast} />)
+            toasts.map(toast => 
+                <CSSTransition
+                    appear
+                    timeout={300}
+                    classNames={{
+                        enter: styles.Appear,
+                        enterActive: styles.AppearActive,
+                        exit: styles.Leave,
+                        exitActive: styles.LeaveActive,
+                    }}
+                    unmountOnExit
+                >
+                    <Toast key={toast._id} toast={toast} />
+                </CSSTransition>
+            )
         }
-    </CSSTransitionGroup>
+    </TransitionGroup>
+
 );
 
 const mapStateToProps = state => ({
