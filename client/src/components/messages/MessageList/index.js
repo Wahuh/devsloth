3bea10f70f338e35471573a7fe220d78e759f5d2
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 21:00:37 +0100

    refactor to hooks component

--- client/src/components/messages/MessageList/index.js
+++ client/src/components/messages/MessageList/index.js
@@ -3,110 +3,50 @@ import { getChannelMessages } from "../duck/selectors";
 import { getMemberAlias } from "../../members/duck/selectors";
 import dateApi from "../../../api/dateApi";
 
-import React, { Component, useRef } from "react";
-// import { VariableSizeList as List } from 'react-window';
-import List from "../../reuse/List";
-import { Scrollbars } from "react-custom-scrollbars";
-import Message from "../Message";
-import ListItem from "../../reuse/ListItem";
+import React from "react";
 import styles from "./MessageList.scss";
 import AutoSizer from "react-virtualized-auto-sizer";
+import InnerMesageList from "./InnerMessageList";
 
-const measure = document.getElementById("measure");
-// measure.classList.add(taskStyles.TextMock);
-const message = document.getElementById("message");
-const PADDING_BOTTOM = 16;
-// const MessageList = ({ messages }) => {
-//     const listRef = useRef(null);
+const messageList = document.getElementById("MessageList");
 
-//     const Row = ({ index, style }) => {
-//         <div style={style}>
-//             <Message message={messages[index]} />
-//         </div>
-//     }
-
-//     const getItemSize = index => {
-//         message.innerHTML = messages[index].text;
-//         const height = message.clientHeight;
-//         message.innerHTML = "";
-//         return height + 18;
-//     }
-
-//     return (
-//         <div className={styles.MessageList}>
-//             <AutoSizer>
-//                 {({ height, width }) => (
-//                     <List
-//                         ref={listRef}
-//                         width={width}
-//                         height={height}
-//                         itemCount={messages.length}
-//                         itemSize={getItemSize}
-//                     >
-//                         {Row}
-//                     </List>
-//                 )}
-//             </AutoSizer>
-//         </div>
-//     );
-// }
-
-class MessageList extends Component {
-    //let messages = props.messages;
-
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-        document.getElementById("Filler").scrollIntoView({ behavior: "smooth" });
-    }
-
-    componentDidUpdate() {
-        document.getElementById("Filler").scrollIntoView({ behavior: "smooth" });
-    }
-
-    render() {
-        const { messages } = this.props;
-        
-        return (
-            <div className={styles.MessageList}>
-                <List>
-                    {messages && messages.map(
-                        message => 
-                        <ListItem>
-                            <Message message={message} />
-                        </ListItem>
-                    )}
-                    <div id="Filler"></div>
-                </List>
-            </div>
-        );
-    }
+const MessageList = ({ messages }) => {
+    return (
+        <div className={styles.MessageList}>
+            <AutoSizer>
+                {({ height, width }) => (
+                    <InnerMesageList 
+                        messages={messages}
+                        height={height}
+                        width={width}
+                    />
+                )}
+            </AutoSizer>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
     const channelMessages = getChannelMessages(state);
+    console.log(channelMessages);
+
     const messages = [];
     let prevMemberId;
     for (let i = 0; i < channelMessages.length; i++) {
         let message = channelMessages[i];
         const { member, text, timestamp, isNotification } = message;
-        if (isNotification) {
-            messages.push(message);
+        if (member === prevMemberId) {
+            messages[messages.length - 1].text += `\n${text}`;
         } else {
-            if (member === prevMemberId) {
-                messages[messages.length - 1].text += `\n${text}`;
-            } else {
-                messages.push({ 
-                    ...message,
-                    timestamp: dateApi.toDate(timestamp), 
-                    alias: getMemberAlias(state, member) 
-                });
-                prevMemberId = member;
-            }
+            messages.push({ 
+                ...message,
+                timestamp: dateApi.toDate(timestamp), 
+                alias: getMemberAlias(state, member) 
+            });
+            prevMemberId = member;
         }
     }
     return { messages };
 } 
+
 export default connect(mapStateToProps)(MessageList);
