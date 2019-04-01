9637ebeb9caaedce60d0b24964cd24428138d362
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:43:39 +0100

    change side menu slide animation

--- client/src/components/ui/HamburgerButton/index.js
+++ client/src/components/ui/HamburgerButton/index.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 import Button from "../../reuse/Button";
 import HamburgerIcon from "../../reuse/icons/MenuIcon";
 import menuStyles from "../../layout/SideMenu/SideMenu.scss";
 import styles from "./HamburgerButton.scss";
+import { toggleSideMenu } from "../duck/actions";
 
-const HamburgerButton = () => (
+
+
+const HamburgerButton = ({ onShow }) => (
     <Button onClick={() => {
-        document.getElementById(menuStyles.SideMenuContainer).style.visibility = "visible";
-        document.getElementById(menuStyles.SideMenuOverlay).style.visibility = "visible";
-        document.getElementById(menuStyles.SideMenu).style.transform = "translate3d(0, 0, 0)";
+        const sideMenu = document.getElementById(menuStyles.SideMenu);
+    const sideMenuOverlay = document.getElementById(menuStyles.SideMenuOverlay);
+        sideMenu.style.transform = "translate3d(0, 0, 0)";
+        sideMenuOverlay.style.visibility = "visible";
+        sideMenuOverlay.style.opacity = 1;
+        onShow(true);
     }} theme="icon" className={styles.HamburgerButton}>
         <HamburgerIcon />
     </Button>
 );
 
-export default HamburgerButton;
+export default connect(null, {
+    onShow: toggleSideMenu
+})(HamburgerButton);
