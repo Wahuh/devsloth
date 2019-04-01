6455d0677c5d6c0232cbb4f2e534299dab73a8d5
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:38:28 +0100

    add onHide callback

--- client/src/components/ui/duck/hooks.js
+++ client/src/components/ui/duck/hooks.js
@@ -1,12 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 
-export function useComponentVisible(initialIsVisible) {
+export function useComponentVisible(initialIsVisible, onHide) {
     const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
     const ref = useRef(null);
 
     const handleClickOutside = (event) => {
         if (ref.current && !ref.current.contains(event.target)) {
             setIsComponentVisible(false);
+            onHide && onHide();
         }
     };
 
