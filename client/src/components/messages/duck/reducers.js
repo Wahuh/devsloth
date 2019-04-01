0ee40b3fb1b56d0745bf5bf7221f5b9f8e80e856
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:57:00 +0100

    load messages

--- client/src/components/messages/duck/reducers.js
+++ client/src/components/messages/duck/reducers.js
@@ -1,15 +1,18 @@
 import { combineReducers } from "redux";
 import { handleActions } from "redux-actions";
 import { receiveMessage } from "./actions";
+import { loadUserData } from "../../user/duck/actions";
 
 export const byId = handleActions(
     {
+        [loadUserData]: (state, { payload }) => addMessage(state, payload),
         [receiveMessage]: (state, { payload }) => addMessage(state, payload),
     }, {}
 );
 
 export const allIds = handleActions(
     {
+        [loadUserData]: (state, { payload }) => addMessageIds(state, payload),
         [receiveMessage]: (state, { payload }) => addMessageId(state, payload),
     }, []
 );
@@ -22,6 +25,13 @@ const addMessage = (state, { entities }) => {
 const addMessageId = (state, { result: messageId }) => {
     return [ ...state, messageId ];
 }
+
+const addMessageIds = (state, { entities }) => {
+    const { messages } = entities;
+    const messageIds = Object.keys(messages);
+    return [ ...state, ...messageIds ];
+}
+
 
 export default combineReducers({
     byId,
