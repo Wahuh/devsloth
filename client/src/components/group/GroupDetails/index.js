62cc8581d423f3e4c0816769c12d4a059d4d9ed0
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:46:15 +0100

    mounting animation

--- client/src/components/group/GroupDetails/index.js
+++ client/src/components/group/GroupDetails/index.js
@@ -2,40 +2,88 @@ import { connect } from "react-redux";
 import { 
     getSelectedGroup
 } from "../duck/selectors";
-import { addUiModal } from "../../ui/duck/actions";
+import { addUiPortal } from "../../ui/duck/actions";
 
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
 import Button from "../../reuse/Button";
-import SettingsIcon from "../../reuse/icons/SettingsIcon";
-import Column from "../../reuse/Column";
 import Typography from "../../reuse/Typography";
 import styles from "./GroupDetails.scss";
-import Tooltip from "../../reuse/Tooltip";
 import { MODAL_GROUP_SETTINGS } from "../../ui/constants";
-import MemberIcon from "../../reuse/icons/MemberIcon";
-import Row from "../../reuse/Row";
+import CloseIcon from "../../reuse/icons/CloseIcon";
+import ArrowDownIcon from "../../reuse/icons/ArrowDownIcon";
 import Icon from "../../reuse/Icon";
+import GroupDropdown from "../GroupDropdown";
+import classNames from "classnames";
 
-const GroupDetails = ({ group, onShowModal }) => {
+const GroupDetails = ({ group }) => {
+    const ref = useRef(null);
+    const [ isDropdownVisible, setIsDropdownVisible ] = useState(false);
+    const [ position, setPosition ] = useState({});
     const { name, members } = group;
 
+    useEffect(() => {
+        const rect = ref.current.getBoundingClientRect();
+        setPosition({ x: rect.left + 8, y: rect.bottom + 8 });
+    }, [])
+
+    const handleClick = event => {
+        setIsDropdownVisible(!isDropdownVisible);
+    }
+
+    const handleClose = event => {
+        setIsDropdownVisible(false);
+    }
+
     return (
-        <div className={styles.GroupDetails}>
+        <div onClick={handleClick} ref={ref} className={styles.GroupDetails}>
             <div className={styles.GroupName}>
                 <Typography color="secondary" type="subheading">
                     {name}
                 </Typography>
             </div>
-            
-            <div data-tip data-for="GroupSettingsIcon" className={styles.ButtonContainer}>
-                <Button onClick={onShowModal} theme="icon">
-                    <Icon size="md">
-                        <SettingsIcon />
-                    </Icon>
 
-                </Button>
-                <Tooltip id="GroupSettingsIcon" message="Group Settings" />
-            </div>
+
+                <div data-tip data-for="GroupSettingsIcon" className={classNames(
+                    styles.ButtonContainer
+                )}>
+
+                    <Button theme="icon">
+                        <Icon size="md">
+                        {isDropdownVisible ?
+                            <CSSTransition
+                                in={isDropdownVisible}
+                                appear
+                                timeout={200}
+                                classNames={{
+                                    enter: styles.Enter,
+                                    enterActive: styles.EnterActive,
+                                    exit: styles.Exit,
+                                    exitActive: styles.ExitActive,
+                                }}
+                            >
+                                    <CloseIcon />
+                            </CSSTransition> :
+                            <CSSTransition
+                                in={isDropdownVisible}
+                                appear
+                                timeout={200}
+                                classNames={{
+                                    enter: styles.Enter,
+                                    enterActive: styles.EnterActive,
+                                    exit: styles.Exit,
+                                    exitActive: styles.ExitActive,
+                                }}
+                            >
+                                    <ArrowDownIcon />
+                            </CSSTransition>
+                        }
+                        </Icon>
+
+                    </Button>
+                </div>
+
+            {isDropdownVisible && <GroupDropdown onHide={handleClose} position={position} />} 
         </div>
     );
 }
@@ -44,6 +92,4 @@ const mapStateToProps = (state, ownProps) => ({
     group: getSelectedGroup(state)
 });
 
-export default connect(mapStateToProps, {
-    onShowModal: () => addUiModal(MODAL_GROUP_SETTINGS)
-})(GroupDetails);
+export default connect(mapStateToProps)(GroupDetails);
