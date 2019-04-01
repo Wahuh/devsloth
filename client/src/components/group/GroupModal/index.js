40a8a6ac15c5511411161b08cf471e15bbdaa85b
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:46:56 +0100

    pass down in prop

--- client/src/components/group/GroupModal/index.js
+++ client/src/components/group/GroupModal/index.js
@@ -1,17 +1,11 @@
-import { connect } from "react-redux";
-import { removeUiModal } from "../../ui/duck/actions";
-
 import React, { useState } from "react";
 import CloseButton from "../../reuse/buttons/CloseButton";
 import GroupCreateOrJoin from "../GroupCreateOrJoin";
 import GroupCreateForm from "../GroupCreateForm";
 import GroupJoinForm from "../GroupJoinForm";
 import Modal from "../../reuse/Modal";
-import { MODAL_GROUP_CREATE_OR_JOIN } from "../../ui/constants";
- 
 
-
-const GroupModal = ({ onHide }) => {
+const GroupModal = ({ onHide, in: inProp }) => {
     const [ screen, setScreen ] = useState("createOrJoin");
 
     const screens = {
@@ -24,13 +18,11 @@ const GroupModal = ({ onHide }) => {
     }
 
     return (
-        <Modal size="md" onHide={onHide}>
+        <Modal size="md" onHide={onHide} in={inProp}>
             <CloseButton onClick={onHide} />
             {screens[screen]}
         </Modal>
     );
 }
 
-export default connect(null, {
-    onHide: () => removeUiModal(MODAL_GROUP_CREATE_OR_JOIN)
-})(GroupModal);
+export default GroupModal;
