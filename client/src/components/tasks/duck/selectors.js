fc209bd59c264e46177c20e649ded37f0a3382bf
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:37:22 +0100

    add new selectors

--- client/src/components/tasks/duck/selectors.js
+++ client/src/components/tasks/duck/selectors.js
@@ -1,5 +1,18 @@
 
-export const getTask = (state, taskId) => state.tasks.byId[taskId];
+export const getTask = (state, props) => state.tasks.byId[props.taskId];
+export const getPreviousTask = (state, props) => {
+    const tasks = Object.values(state.tasks.byId);
+    return tasks.find(task => task.next === props.taskId);
+}
+export const getHeadTask = (state, props) => {
+    const tasks = Object.values(state.tasks.byId);
+    return tasks.find(task => task.next === props);
+}
 
+export const getSelectedTaskName = state => {
+    const taskId = getSelectedTaskId(state);
+    if (taskId) return state.tasks.byId[taskId].name;
+    return null;
+};
 export const getSelectedTaskId = state => state.tasks.selectedId;
 export const getSelectedTask = ({ tasks }) => tasks.selectedId ? tasks.byId[tasks.selectedId] : null; 
