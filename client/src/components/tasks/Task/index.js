960ef97e416128627aba498a7644378091159f49
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:37:43 +0100

    open task dropdown

--- client/src/components/tasks/Task/index.js
+++ client/src/components/tasks/Task/index.js
@@ -1,54 +1,62 @@
 import { connect } from "react-redux";
-import { addUiModal } from "../../ui/duck/actions";
+import { addUiPortal } from "../../ui/duck/actions";
 
-import React, { useState, useRef } from "react";
-import { NavLink, withRouter } from "react-router-dom";
-import BulletListIcon from "../../reuse/icons/BulletListIcon";
+import React, { useEffect, useRef } from "react";
 import Button from "../../reuse/Button";
-import ChatBubbleIcon from "../../reuse/icons/ChatBubbleIcon";
-import DeleteButton from "../../reuse/buttons/DeleteButton";
 import EditIcon from "../../reuse/icons/EditIcon";
 import Typography from "../../reuse/Typography";
 import styles from "./Task.scss";
-import { MODAL_TASK_EDIT, MODAL_TASK_DROPDOWN } from "../../ui/constants";
+import { MODAL_TASK_EDIT, DROPDOWN_TASK } from "../../ui/constants";
 import { selectTask } from "../duck/actions";
-import TaskDropdown from "../TaskDropdown";
-import { getTask } from "../duck/selectors";
+import useDraggable from "../../dnd/useDraggable";
 
-export const Task = ({ task, onEdit, onSelect, match }) => {
-    const taskRef = useRef(null);
-    const [showDropdown, setShowDropdown] = useState(false);
-    
-    const [position, setPosition] = useState(null);
+
+
+export const Task = (props) => {
+    const { task, onEdit, onSelect, match, index, listId, onDropdown } = props;
     const { _id } = task;
 
-    const onDropdown = () => {
-        onSelect(_id);
-        const clientRect = taskRef.current.getBoundingClientRect()
-        setPosition({ top: clientRect.top + document.documentElement.scrollTop, left: clientRect.left }  );
-        setShowDropdown(true);
+    const [ { isDragging }, ref ] = useDraggable({ 
+      draggableId: _id,
+      droppableId: listId,
+      index
+    });
+    // useWhyDidYouUpdate("TASK", props);
+
+    function handleDropdown() {
+      const rect = ref.current.getBoundingClientRect();
+		onDropdown({
+			portalProps: {
+				taskId: _id,
+        position: { x: rect.left + document.documentElement.scrollTop, y: rect.top },
+        index 
+      },
+      portalDuration: 200,
+			portalType: DROPDOWN_TASK
+		});
     }
 
-    const onClick = event => {
-        event.preventDefault();
-        onSelect(_id);
-        onEdit();
-    };
-
     return (
-        <ul ref={taskRef} className={styles.Task}>
-            <NavLink to={`${match.url}/${_id}`} onClick={onClick}>
+        <li ref={ref} className={styles.Task}>
+            <span>
                 <Typography color="primary">
                     {task.name}
                 </Typography>
-            </NavLink>
-            <Button onClick={onDropdown} type="button" className={styles.TaskEditButton}>
+            </span>
+
+            <Button onClick={handleDropdown} type="button" className={styles.TaskEditButton}>
                 <EditIcon />
             </Button>
-            {showDropdown && <TaskDropdown onHide={() => setShowDropdown(false)} position={position} />}
-        </ul>
+        </li>
     )
 }
+
+// const onDropdown = () => {
+//   onSelect(_id);
+//   const clientRect = taskRef.current.getBoundingClientRect()
+//   setPosition({ top: clientRect.top + document.documentElement.scrollTop, left: clientRect.left }  );
+//   setShowDropdown(true);
+// }
 
 const makeMapStateToProps = (initialState, initialProps) => {
     const { taskId } = initialProps;
@@ -61,10 +69,42 @@ const makeMapStateToProps = (initialState, initialProps) => {
 }
 
 
-export default withRouter(
-    connect(makeMapStateToProps, {
-        onEdit: () => addUiModal(MODAL_TASK_EDIT),
-        onSelect: selectTask,
-        onShowModal: () => addUiModal(MODAL_TASK_DROPDOWN)
-    })(Task)
-);
+export default connect(makeMapStateToProps, {
+    onEdit: () => addUiPortal({ portalType: MODAL_TASK_EDIT }),
+    onSelect: selectTask,
+    onDropdown: addUiPortal
+})(Task)
+
+function useWhyDidYouUpdate(name, props) {
+    // Get a mutable ref object where we can store props ...
+    // ... for comparison next time this hook runs.
+    const previousProps = useRef();
+  
+    useEffect(() => {
+      if (previousProps.current) {
+        // Get all keys from previous and current props
+        const allKeys = Object.keys({ ...previousProps.current, ...props });
+        // Use this object to keep track of changed props
+        const changesObj = {};
+        // Iterate through keys
+        allKeys.forEach(key => {
+          // If previous is different from current
+          if (previousProps.current[key] !== props[key]) {
+            // Add to changesObj
+            changesObj[key] = {
+              from: previousProps.current[key],
+              to: props[key]
+            };
+          }
+        });
+  
+        // If changesObj not empty then output to console
+        if (Object.keys(changesObj).length) {
+          console.log('[why-did-you-update]', name, changesObj);
+        }
+      }
+  
+      // Finally update previousProps with current props for next hook call
+      previousProps.current = props;
+    });
+  }
