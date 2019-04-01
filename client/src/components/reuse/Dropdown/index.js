b951676fb47622cdcc780a4e5a2ff4f62c7c78f6
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:58:35 +0100

    portal components

--- client/src/components/reuse/Dropdown/index.js
+++ client/src/components/reuse/Dropdown/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import classNames from "classnames";
 import styles from "./Dropdown.scss";
 import { useComponentVisible } from "../../ui/duck/hooks";
+import Overlay from "../Overlay";
+import Portal from "../Portal";
 
-const Dropdown = ({ children }) => {
-    const { ref, isComponentVisible } = useComponentVisible(true);
 
-    return (
-        <ul className={styles.Dropdown} ref={ref}>
-            {isComponentVisible && children}
+const Dropdown = ({ children, position, onHide, overlay }) => {
+    const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(true);
+
+    useEffect(() => {
+        if (!isComponentVisible && onHide) {
+            onHide()
+        }
+    }, [ isComponentVisible ])
+
+    let dropdown = (
+        <ul 
+            style={position && { left: position.x, top: position.y }} 
+            className={classNames(styles.Dropdown)} 
+            ref={ref}
+        >
+            {children}
         </ul>
+    );
+    return (
+        <Portal>
+            {overlay ? (
+                <Overlay duration={200} isVisible={isComponentVisible}>
+                    {dropdown}
+                </Overlay>
+            ) : dropdown
+            }
+        </Portal>
     );
 }
 
