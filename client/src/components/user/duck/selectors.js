ba83b48d48ef3e649b1e240af223cfeac09f7f68
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:42:34 +0100

    add username and userlists selectors

--- client/src/components/user/duck/selectors.js
+++ client/src/components/user/duck/selectors.js
@@ -1,3 +1,4 @@
 export const getUserId = state => state.user.id;
 export const getUserEmail = state => state.user.email;
 export const getUsername = state => state.user.username;
+export const getUserListIds = state => state.user.lists;
