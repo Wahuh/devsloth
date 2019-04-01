1af790287e9c9572549aaa8531420155db2cf7df
Thanh Doan <tmdoan98@gmail.com>
2019-04-01 20:59:45 +0100

    simplify styling

--- client/src/components/messages/Message/index.js
+++ client/src/components/messages/Message/index.js
@@ -1,44 +1,41 @@
 import React from "react";
-import * as linkify from "linkifyjs";
-import hashtag from "linkifyjs/plugins/hashtag";
 import Linkify from "linkifyjs/react";
-
-import Typography from "../../reuse/Typography";
-import Row from "../../reuse/Row";
 import styles from "./Message.scss";
 import NotificationMessage from "./NotificationMessage";
 
 const Message = ({ message }) => {
     const { alias, text, icon, timestamp, isNotification } = message;
-    return isNotification ? (
-        <NotificationMessage>
-            {text}
-        </NotificationMessage>
-    ) : 
-    (<li className={styles.Message}>
-        <div className={styles.MessageIcon}>
-        </div>
+    // return isNotification ? (
+    //     <NotificationMessage text={text} />
+    // ) : 
+    // (
+    return (
+        <li className={styles.Message}>
+            <div className={styles.MessageIcon}></div>
 
-        <div className={styles.MessageBody}>
-            <Row alignItems="flex-end">
-                <div className={styles.MessageAlias}>
-                    <Typography color="secondary" bold>{alias}</Typography>
+            <div className={styles.MessageBody}>
+                <span>
+                    <p>
+                        {alias}
+                    </p>
+
+                    <small>
+                        {timestamp}
+                    </small>
+                </span>
+
+                <div className={isNotification && styles.NotificationText}>
+                    <Linkify>
+                        <p>
+                            {text}
+                        </p>
+                    </Linkify>
                 </div>
-
-                <div className={styles.MessageTimestamp}>
-                    <Typography color="tertiary" type="caption">{timestamp}</Typography>
-                </div>
-            </Row>
-
-            <div className={styles.MessageText}>
-                <Linkify>
-                    <Typography color="primary" type="body">
-                        {text}
-                    </Typography>
-                </Linkify>
             </div>
-        </div>
-    </li>)
+        </li>
+        );
 }
 
 export default Message;
+
+//24 x24 padd, 16 x 16 marg y, 568 x 72, 464x108
