6aac9117268a1dbdd5d8d8e2dd623a50cda56242
Thanh Doan <tmdoan98@gmail.com>
2019-08-27 21:55:21 +0100

    add disabled prop

--- client/src/common/components/Button/index.jsx
+++ client/src/common/components/Button/index.jsx
@@ -3,11 +3,12 @@ import classNames from 'classnames';
 import Proptypes from 'prop-types';
 import styles from './Button.module.scss';
 
-const Button = ({children, className, onClick}) => {
+const Button = ({children, className, onClick, disabled}) => {
   return (
     <button
       onClick={onClick}
       type="button"
+      disabled={disabled}
       className={classNames(styles.Button, className)}
     >
       {children}
@@ -18,12 +19,14 @@ const Button = ({children, className, onClick}) => {
 Button.defaultProps = {
   children: null,
   className: null,
+  disabled: false,
 };
 
 Button.propTypes = {
   children: Proptypes.node,
   className: Proptypes.string,
   onClick: Proptypes.func.isRequired,
+  disabled: Proptypes.bool,
 };
 
 export default Button;
