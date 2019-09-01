2828383c69bae645a719639915cf55edf18163f4
Thanh Doan <tmdoan98@gmail.com>
2019-09-01 20:51:11 +0100

    add handler for loginSuccess action

--- client/src/auth/redux/reducers.js
+++ client/src/auth/redux/reducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import {handleActions} from 'redux-actions';
 import {signupSuccess} from '../../signup/redux/actions';
+import {loginSuccess} from '../../login/redux/actions';
 
 export const isAuthenticated = handleActions(
   {
     [signupSuccess]: () => true,
+    [loginSuccess]: () => true,
   },
   false,
 );
