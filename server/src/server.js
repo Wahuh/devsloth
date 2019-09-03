6cc8c8dc5314d78c4a8a54cefd02949852b7345f
Thanh Doan <tmdoan98@gmail.com>
2019-09-03 20:02:58 +0100

    cleanup

--- server/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const socketIo = require("socket.io");
-
-const server = app.listen(process.env.PORT);
-const io = socketIo(server);
-require("./bin/socket")(io);
-app.set("io", io);
