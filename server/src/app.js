9fec2323161051c0077cf612d6f60547acb6b596
Thanh Doan <tmdoan98@gmail.com>
2019-09-03 20:02:58 +0100

    cleanup

--- server/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-require("./bin/env")();
-require("./bin/cors")(app);
-require("./bin/routes")(app);
-require("./bin/db")();
-
-module.exports = app;
