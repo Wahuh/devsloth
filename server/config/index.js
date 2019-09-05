33c12e14c785922f2d512055972ce1bdc8ab43ad
Thanh Doan <tmdoan98@gmail.com>
2019-09-05 22:29:36 +0100

    setup & connect to test postgres db in docker container

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgDatabase: process.env.POSTGRES_DATABASE,
+  pgUser: process.env.POSTGRES_USER,
+  pgPort: process.env.POSTGRES_PORT,
+  pgHost: process.env.POSTGRES_HOST,
+};
