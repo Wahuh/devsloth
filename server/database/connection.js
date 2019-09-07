b324ed523acb9e722deb9ad3bba9e773c8eb1016
Thanh <tmdoan98@gmail.com>
2019-09-07 14:32:58 +0100

    add objection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,8 @@
 const knex = require('knex');
-
+const {Model} = require('objection');
 const dbConfig = require('../knexfile');
 
 const connection = knex(dbConfig);
+Model.knex(connection);
 
 module.exports = connection;
