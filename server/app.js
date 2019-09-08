805f993ca173789cd34a220ca68384c60747b740
Thanh <tmdoan98@gmail.com>
2019-09-08 13:10:03 +0100

    add centralized error handling

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const Koa = require('koa');
 const mount = require('koa-mount');
 const bodyParser = require('koa-bodyparser');
+const handleErrors = require('./middleware/errors');
 
 const app = new Koa();
 const signup = require('./lib/signup');
 
 app.use(bodyParser());
 app.use(mount('/signup', signup));
+app.on('error', handleErrors);
 
 module.exports = app;
