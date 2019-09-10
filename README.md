d8090f85984f92a6e2c2415c86d602b1d10776dc
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "add new sections"
    This reverts commit 0f4183f07d494994038d776b1b6527307da1234e.

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
-# Slothy.io
-
-A web app inspired by Slack, Discord and Trello which features chat, task management and social groups.
-
-## Features
-
-## Getting Started
-
-### Prerequisites
-
-* Docker installed
-* Linux
-
-### Installation
-
-
-
 ## Running tests
 
 ```
--- server/lib/signup/routes.js
+++ server/lib/signup/routes.js
@@ -1,13 +1,5 @@
 const Router = require('@koa/router');
 
 const signupRouter = new Router();
-signupRouter.get('/', ctx => {
-  console.log('cn')
-  ctx.body = 'g'
-})
-signupRouter.post('/', ctx => {
-  ctx.response.status = 201;
-  ctx.body = "hello2saasdasd";
-});
 
 module.exports = signupRouter;
