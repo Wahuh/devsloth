938504ea1e05922108bb5efa2fbcfecbf840c722
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "add separate config with seeds for development"
    This reverts commit 5b96bfd4b6a4e0589a2aa847fff58c62c3221819.

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,32 +1,14 @@
-const {pgHost, pgDatabase, pgUser, pgPassword, env} = require('./config');
+const {pgHost, pgDatabase, pgUser, pgPassword} = require('./config');
 
-if (env === 'development') {
-  module.exports = {
-    client: 'pg',
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
-    connection: {
-      host: pgHost,
-      database: pgDatabase,
-      user: pgUser,
-      password: pgPassword,
-    },
-  };
-} else {
-  module.exports = {
-    client: 'pg',
-    migrations: {
-      directory: './database/migrations',
-    },
-    connection: {
-      host: pgHost,
-      database: pgDatabase,
-      user: pgUser,
-      password: pgPassword,
-    },
-  };
-}
+module.exports = {
+  client: 'pg',
+  migrations: {
+    directory: './database/migrations',
+  },
+  connection: {
+    host: pgHost,
+    database: pgDatabase,
+    user: pgUser,
+    password: pgPassword,
+  },
+};
