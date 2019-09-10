12989864b515d259586d6f10a651158919002240
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "add centralized error handling"
    This reverts commit 805f993ca173789cd34a220ca68384c60747b740.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,11 @@
 const Koa = require('koa');
 const mount = require('koa-mount');
 const bodyParser = require('koa-bodyparser');
-const handleErrors = require('./middleware/errors');
 
 const app = new Koa();
 const signup = require('./lib/signup');
 
 app.use(bodyParser());
 app.use(mount('/signup', signup));
-app.on('error', handleErrors);
 
 module.exports = app;
