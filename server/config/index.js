7681a88d510834e038809e31980937616f730b24
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "setup & connect to test postgres db in docker container"
    This reverts commit 33c12e14c785922f2d512055972ce1bdc8ab43ad.

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  pgPassword: process.env.POSTGRES_PASSWORD,
-  pgDatabase: process.env.POSTGRES_DATABASE,
-  pgUser: process.env.POSTGRES_USER,
-  pgPort: process.env.POSTGRES_PORT,
-  pgHost: process.env.POSTGRES_HOST,
-};
