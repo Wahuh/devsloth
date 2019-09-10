92963bcae3c0ad384797c9e9ca05faebf449d468
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "add signup app"
    This reverts commit c5f207a8938d8ca0771abd12d212b38e7ee19088.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,8 @@
 const Koa = require('koa');
-const mount = require('koa-mount');
-const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
-const signup = require('./lib/signup');
+const bodyParser = require('koa-bodyparser');
 
-app.use(bodyParser());
-app.use(mount('/signup', signup));
+app.use(bodyParser);
 
 module.exports = app;
--- server/lib/signup/routes.js
+++
@@ -1,5 +0,0 @@
-const Router = require('@koa/router');
-
-const signupRouter = new Router();
-
-module.exports = signupRouter;
