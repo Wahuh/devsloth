d9d2fe07270e99ea9b5e862ddb632c6c7a5d0490
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "add objection.js"
    This reverts commit b324ed523acb9e722deb9ad3bba9e773c8eb1016.

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,8 +1,7 @@
 const knex = require('knex');
-const {Model} = require('objection');
+
 const dbConfig = require('../knexfile');
 
 const connection = knex(dbConfig);
-Model.knex(connection);
 
 module.exports = connection;
