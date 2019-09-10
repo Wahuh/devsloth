9b8096978d253a43b1b913ea689deda236d00145
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "rename to POSTGRES_DB + set env to development by default"
    This reverts commit 556e5bbcd4ae13bc25a7fbe149df03f5f9415760.

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,8 @@
 module.exports = {
   pgPassword: process.env.POSTGRES_PASSWORD,
-  pgDatabase: process.env.POSTGRES_DB,
+  pgDatabase: process.env.POSTGRES_DATABASE,
   pgUser: process.env.POSTGRES_USER,
   pgPort: process.env.POSTGRES_PORT,
   pgHost: process.env.POSTGRES_HOST,
   secret: process.env.JWT_SECRET,
-  env: process.env.NODE_ENV || 'development',
 };
