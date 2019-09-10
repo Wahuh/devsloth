5ead15a2ac18e720b40f78e0cf77d8bc6df0014c
Wahuh <31378256+Wahuh@users.noreply.github.com>
2019-09-10 16:11:48 +0100

    Revert "install koa2-cors"
    This reverts commit 9ca54c2e0dc1404057ac77965d12b8121a570a7b.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,11 @@
 const Koa = require('koa');
 const mount = require('koa-mount');
-const cors = require('koa2-cors');
 const bodyParser = require('koa-bodyparser');
 const handleErrors = require('./middleware/errors');
 
 const app = new Koa();
 const signup = require('./lib/signup');
 
-app.use(cors());
 app.use(bodyParser());
 app.use(mount('/signup', signup));
 app.on('error', handleErrors);
