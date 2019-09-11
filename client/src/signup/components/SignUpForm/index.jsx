10503457505bea1db49bc8f24bd83f9131d346cf
Thanh <tmdoan98@gmail.com>
2019-09-11 11:07:10 +0100

    add letterSpacing to h1

--- client/src/signup/components/SignUpForm/index.jsx
+++ client/src/signup/components/SignUpForm/index.jsx
@@ -76,7 +76,14 @@ const SignUpForm = ({onSignup}) => {
 
   return (
     <form onSubmit={handleSubmit} className={styles.SignUpForm}>
-      <Typography as="h1" mb={48} color="primary" fontSize={36}>
+      <Typography
+        as="h1"
+        mb={48}
+        fontWeight={700}
+        letterSpacing="0.5px"
+        color="primary"
+        fontSize={36}
+      >
         Slothy.io
       </Typography>
 
