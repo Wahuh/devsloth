6cb5a193bcdc3349fee8144c1fd94cbe0c02e1c6
Thanh <tmdoan98@gmail.com>
2019-09-14 18:11:29 +0100

    add /me subapp

--- server/lib/api/index.js
+++ server/lib/api/index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const mount = require('koa-mount');
+
 const api = new Koa();
 
+const me = require('../me');
 const signup = require('../signup');
 
+api.use(mount('/me', me));
 api.use(mount('/signup', signup));
 
 module.exports = api;
