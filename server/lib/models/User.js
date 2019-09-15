be97bcd8e0789a47744ed6b38de702ba810b2340
Thanh <tmdoan98@gmail.com>
2019-09-15 12:02:06 +0100

    add boards relation + findBoards instance method

--- server/lib/models/User.js
+++ server/lib/models/User.js
@@ -13,6 +13,34 @@ class User extends Model {
     return {username, email, id};
   }
 
+  static get relationMappings() {
+    // eslint-disable-next-line
+    const Board = require('./Board');
+
+    return {
+      boards: {
+        relation: Model.HasManyRelation,
+        modelClass: Board,
+        filter: {owner_type: 'user'},
+        join: {
+          from: 'users.id',
+          to: 'boards.owner_id',
+        },
+      },
+    };
+  }
+
+  async findBoards() {
+    try {
+      const boards = await this.$relatedQuery('boards').where({
+        owner_type: 'user',
+      });
+      return boards;
+    } catch (err) {
+      return Promise.reject(new Error('Boards not found'));
+    }
+  }
+
   static async findById(id) {
     try {
       const user = await this.query().findById(id);
