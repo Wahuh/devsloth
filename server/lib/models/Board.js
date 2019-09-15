73114000190b3cf8332c0c58ed606a255fd4f830
Thanh <tmdoan98@gmail.com>
2019-09-15 15:16:45 +0100

    add findById and update methods

--- server/lib/models/Board.js
+++ server/lib/models/Board.js
@@ -9,6 +9,28 @@ class Board extends Model {
     const insertedBoard = await this.query().insert(board);
     return insertedBoard;
   }
+
+  static async findById(id) {
+    const board = await this.query().findById(id);
+    if (!board) {
+      const error = new Error('board not found');
+      error.name = 'E200';
+      return Promise.reject(error);
+    }
+    return board;
+  }
+
+  async update(board) {
+    try {
+      const updatedBoard = await this.$query()
+        .update(board)
+        .where({id: this.id})
+        .returning('*');
+      return updatedBoard;
+    } catch (err) {
+      return Promise.reject(new Error('board update failed'));
+    }
+  }
 }
 
 module.exports = Board;
