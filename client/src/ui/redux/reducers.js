37e279a73a8e5b434e4cf9f8c7b6903951f3baea
Thanh <tmdoan98@gmail.com>
2019-10-14 19:52:32 +0100

    add fetching state to track requests

--- client/src/ui/redux/reducers.js
+++ client/src/ui/redux/reducers.js
@@ -1,16 +1,30 @@
 import {combineReducers} from 'redux';
 import {handleActions} from 'redux-actions';
 import {showModal, hideModal} from './actions';
+import {START_FETCHING, STOP_FETCHING} from './types';
 
 const modals = handleActions(
   {
     [showModal]: (state, {payload}) => [...state, payload],
     [hideModal]: (state, {payload}) => {
-      console.log(payload, state);
       return state.filter(modal => modal !== payload);
     },
   },
   [],
 );
 
-export default combineReducers({modals});
+const fetching = handleActions(
+  {
+    [START_FETCHING]: (state, {payload}) => ({
+      ...state,
+      [payload]: true,
+    }),
+    [STOP_FETCHING]: (state, {payload}) => ({
+      ...state,
+      [payload]: false,
+    }),
+  },
+  {},
+);
+
+export default combineReducers({fetching, modals});
