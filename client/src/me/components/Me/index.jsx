1547d8c1463a54438244384771c1788d50e2740f
Thanh <tmdoan98@gmail.com>
2019-10-14 17:49:36 +0100

    add proptypes

--- client/src/me/components/Me/index.jsx
+++ client/src/me/components/Me/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Route} from 'react-router-dom';
 import Board from '../../../boards/components/Board';
 import BoardsList from '../../../boards/components/BoardsList';
 
 const Me = ({match}) => {
-  console.log(match);
   return (
     <div>
       <Route exact path={`${match.path}/boards`} component={BoardsList} />
       <Route path={`${match.path}/boards/:board_id`} component={Board} />
     </div>
   );
+};
+
+Me.propTypes = {
+  match: PropTypes.objectOf({path: PropTypes.string}).isRequired,
 };
 
 export default Me;
