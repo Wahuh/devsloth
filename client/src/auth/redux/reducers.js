1e6eba9dc16bac0dc3b754f96e9dbb096771387d
Thanh <tmdoan98@gmail.com>
2019-10-14 22:45:07 +0100

    spread user state and new payload

--- client/src/auth/redux/reducers.js
+++ client/src/auth/redux/reducers.js
@@ -1,24 +1,23 @@
 import {combineReducers} from 'redux';
 import {handleActions} from 'redux-actions';
-import {signupSuccess} from '../../signup/redux/actions';
-import {loginSuccess, jwtLoginSuccess} from '../../login/redux/actions';
-import {JWT_LOGIN_SUCCESS} from '../../login/redux/types';
+import {JWT_LOGIN_SUCCESS, LOGIN_SUCCESS} from '../../login/redux/types';
+import {SIGNUP_SUCCESS} from '../../signup/redux/types';
 
 export const isAuthenticated = handleActions(
   {
-    [signupSuccess]: () => true,
-    [loginSuccess]: () => true,
+    [SIGNUP_SUCCESS]: () => true,
+    [LOGIN_SUCCESS]: () => true,
     [JWT_LOGIN_SUCCESS]: () => true,
   },
   false,
 );
 
-const addUser = (state, action) => action.payload;
+const addUser = (state, action) => ({...state, ...action.payload});
 
 export const user = handleActions(
   {
-    [signupSuccess]: addUser,
-    [loginSuccess]: addUser,
+    [SIGNUP_SUCCESS]: addUser,
+    [LOGIN_SUCCESS]: addUser,
     [JWT_LOGIN_SUCCESS]: addUser,
   },
   {},
