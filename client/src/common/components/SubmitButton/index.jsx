b12c913eaa2559a3711adc78a14c4b7ed2bd44c5
Thanh <tmdoan98@gmail.com>
2019-10-14 23:13:44 +0100

    add loading button & close modal after load

--- client/src/common/components/SubmitButton/index.jsx
+++ client/src/common/components/SubmitButton/index.jsx
@@ -3,22 +3,29 @@ import PropTypes from 'prop-types';
 import Button from '../Button';
 import Typography from '../Typography';
 import styles from './SubmitButton.module.scss';
+import Spinner from '../Spinner';
 
-const SubmitButton = ({isFormValid, text}) => (
+const SubmitButton = ({isFormValid, text, isFetching}) => (
   <Button type="submit" disabled={!isFormValid} className={styles.SubmitButton}>
-    <Typography color="complement" as="span">
-      {text}
-    </Typography>
+    {isFetching ? (
+      <Spinner />
+    ) : (
+      <Typography color="complement" as="span">
+        {text}
+      </Typography>
+    )}
   </Button>
 );
 
 SubmitButton.defaultProps = {
   isFormValid: false,
+  isFetching: false,
 };
 
 SubmitButton.propTypes = {
   isFormValid: PropTypes.bool,
   text: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool,
 };
 
 export default SubmitButton;
