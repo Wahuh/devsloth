116d2e1b55af8391d9825b9906cacfe4c92fdd18
Thanh <tmdoan98@gmail.com>
2019-10-14 23:16:47 +0100

    update spinner style

--- client/src/common/components/Spinner/index.jsx
+++ client/src/common/components/Spinner/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import './Spinner.scss';
 
-const Spinner = () => <div>spin</div>;
+const Spinner = () => (
+  <div className="spinner">
+    <div className="bounce1" />
+    <div className="bounce2" />
+    <div className="bounce3" />
+  </div>
+);
 
 export default Spinner;
