d824104cdf6c0c1d6a317404a9c8305c39806a89
Thanh <tmdoan98@gmail.com>
2019-10-16 18:58:13 +0100

    add schema for multiple lists

--- client/src/api/schemas.js
+++ client/src/api/schemas.js
@@ -3,9 +3,11 @@ import {schema} from 'normalizr';
 const board = new schema.Entity('boards');
 const boards = [board];
 const list = new schema.Entity('lists');
+const lists = [list];
 
 export default {
   board,
   boards,
   list,
+  lists,
 };
