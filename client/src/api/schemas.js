656c4953d0f57fa4bd42985023c019b2d0702405
Thanh <tmdoan98@gmail.com>
2019-10-16 22:14:54 +0100

    add schemas for tasks

--- client/src/api/schemas.js
+++ client/src/api/schemas.js
@@ -4,10 +4,14 @@ const board = new schema.Entity('boards');
 const boards = [board];
 const list = new schema.Entity('lists');
 const lists = [list];
+const task = new schema.Entity('tasks');
+const tasks = [task];
 
 export default {
   board,
   boards,
   list,
   lists,
+  task,
+  tasks,
 };
