a536727d836acbf5e6a0f95377b9ffdc8c6bd1a0
Thanh <tmdoan98@gmail.com>
2019-10-16 22:26:12 +0100

    add api for tasks

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,7 @@ import {getBoard, postUserBoard, getUserBoards} from './boards.api';
 import {getUser} from './me.api';
 import {login, signup} from './auth.api';
 import {postList, getLists} from './lists.api';
+import {postTask} from './tasks.api';
 
 export default {
   postList,
@@ -9,8 +10,8 @@ export default {
   postUserBoard,
   getBoard,
   getLists,
-  postTask: () => {},
   getUser,
+  postTask,
   login,
   signup,
 };
