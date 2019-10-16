44824378603f9d430b0fffeff3ed9d5ad9f0646a
Thanh <tmdoan98@gmail.com>
2019-10-16 22:12:56 +0100

    add default value for description so it can't be null

--- server/lib/lists/controllers.js
+++ server/lib/lists/controllers.js
@@ -4,8 +4,8 @@ const postListTask = async ctx => {
   try {
     const {id} = ctx.request.user;
     const {list_id} = ctx.params;
-    const {title, description} = ctx.request.body;
-    const task = await Task.addOne({title, description, list_id: +list_id});
+    const {title} = ctx.request.body;
+    const task = await Task.addOne({title, description: '', list_id: +list_id});
     ctx.app.emit('push', 'task create', task, id);
     ctx.status = 204;
     ctx.body = {};
