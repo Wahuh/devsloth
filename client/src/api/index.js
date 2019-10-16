ad0f672319fbd3529348c7ce5b58009322b598b7
Thanh <tmdoan98@gmail.com>
2019-10-16 18:58:37 +0100

    add api function for getting lists

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,14 @@
 import {getBoard, postUserBoard, getUserBoards} from './boards.api';
 import {getUser} from './me.api';
 import {login, signup} from './auth.api';
-import {postList} from './lists.api';
+import {postList, getLists} from './lists.api';
 
 export default {
   postList,
   getUserBoards,
   postUserBoard,
   getBoard,
+  getLists,
   postTask: () => {},
   getUser,
   login,
