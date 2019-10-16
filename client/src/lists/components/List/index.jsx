1249c6ae1eec0f462d089cb3fb413f78f616e652
Thanh <tmdoan98@gmail.com>
2019-10-16 19:17:51 +0100

    add lists components and basic styling

--- client/src/lists/components/List/index.jsx
+++ client/src/lists/components/List/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '../../../common/components/Typography';
+import styles from './List.module.scss';
 
-const List = ({ list }) => {
+const List = ({list}) => {
+  const {title} = list;
   return (
-    <ul>
-      
-    </ul>
+    <li className={styles.List}>
+      <Typography fontSize={18} fontWeight={600} color="secondary">
+        {title}
+      </Typography>
+      <ul />
+    </li>
   );
+};
+
+List.propTypes = {
+  list: PropTypes.objectOf({title: PropTypes.string}).isRequired,
 };
 
 export default List;
--- client/src/lists/components/Lists/index.jsx
+++ client/src/lists/components/Lists/index.jsx
@@ -3,12 +3,20 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {selectBoardLists} from '../../redux/selectors';
 import {getListsRequest} from '../../redux/actions';
+import List from '../List';
+import styles from './Lists.module.scss';
 
 const Lists = ({lists, onGetLists, board_id}) => {
   useEffect(() => {
     onGetLists(board_id);
   }, []);
-  return <ul>{lists.map(list => list.title)}</ul>;
+  return (
+    <ul className={styles.Lists}>
+      {lists.map(list => (
+        <List list={list} />
+      ))}
+    </ul>
+  );
 };
 
 Lists.propTypes = {
