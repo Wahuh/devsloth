e198b06822c6472cfd366386da9da334f5196139
Thanh <tmdoan98@gmail.com>
2019-10-17 20:59:31 +0100

    add test for tasks actions

--- client/src/tasks/redux/actions.js
+++ client/src/tasks/redux/actions.js
@@ -2,10 +2,10 @@ import {createAction} from 'redux-actions';
 import {CREATE_TASK_REQUEST} from './types';
 import makeRequestMetaCreator from '../../common/redux/makeRequestMetaCreator';
 
+/* eslint-disable */
 export const createTaskRequest = createAction(
   CREATE_TASK_REQUEST,
   null,
   makeRequestMetaCreator('postTask'),
 );
-
-export const getTasks = () => {};
+/* eslint-enable */
