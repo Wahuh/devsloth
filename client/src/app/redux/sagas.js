773d4c6d6a93115238082195ae75e3b1581df4a7
Thanh <tmdoan98@gmail.com>
2019-10-17 19:16:15 +0100

    remove signup sagas

--- client/src/app/redux/sagas.js
+++ client/src/app/redux/sagas.js
@@ -2,11 +2,10 @@ import {all} from 'redux-saga/effects';
 // import boardsSaga from '../../boards/redux/sagas';
 import loginSaga from '../../login/redux/sagas';
 // import meSaga from '../../me/redux/sagas';
-import signupSaga from '../../signup/redux/sagas';
 import commonSaga from '../../common/redux/sagas';
 
 function* rootSaga() {
-  yield all([commonSaga(), loginSaga(), signupSaga()]);
+  yield all([commonSaga(), loginSaga()]);
 }
 
 export default rootSaga;
