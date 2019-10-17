26f1a566e436958bb0504d85593634a0ff4f9c11
Thanh <tmdoan98@gmail.com>
2019-10-17 19:43:13 +0100

    remove loginSaga

--- client/src/app/redux/sagas.js
+++ client/src/app/redux/sagas.js
@@ -1,11 +1,10 @@
 import {all} from 'redux-saga/effects';
 // import boardsSaga from '../../boards/redux/sagas';
-import loginSaga from '../../login/redux/sagas';
 // import meSaga from '../../me/redux/sagas';
 import commonSaga from '../../common/redux/sagas';
 
 function* rootSaga() {
-  yield all([commonSaga(), loginSaga()]);
+  yield all([commonSaga()]);
 }
 
 export default rootSaga;
