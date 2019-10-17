6088583a2d56110a12ab09a84710dcc39d2935f9
Thanh <tmdoan98@gmail.com>
2019-10-17 23:02:14 +0100

    add proptypes and fix import

--- client/src/app/components/App/index.jsx
+++ client/src/app/components/App/index.jsx
@@ -1,19 +1,24 @@
 import React, {useEffect} from 'react';
-import authApi from '../../../api/auth.api';
-import './App.scss';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {getJwt} from '../../../api/auth.api';
+import './App.scss';
 import {jwtLoginRequest} from '../../../login/redux/actions';
 import Slothy from '../Slothy';
 
 const App = ({onJwtLogin}) => {
   useEffect(() => {
-    const jwt = authApi.getJwt();
+    const jwt = getJwt();
     if (jwt) {
       onJwtLogin(jwt);
     }
   }, []);
 
   return <Slothy />;
+};
+
+App.propTypes = {
+  onJwtLogin: PropTypes.func.isRequired,
 };
 
 export default connect(
