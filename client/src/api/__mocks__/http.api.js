fb951c77d31d3338c26de454f738e819c8441d89
Thanh <tmdoan98@gmail.com>
2019-10-18 12:19:37 +0100

    add tests for board api functions

--- client/src/api/__mocks__/http.api.js
+++ client/src/api/__mocks__/http.api.js
@@ -1,8 +1,7 @@
-const baseUrl = process.env.API_URL;
 const bearerToken =
   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTcxMTcyNjE0fQ.Gp-mrmHVnSgluaXZECQC3_mf98P3_nj1jHa22wK7n6M';
 
-const responses = {
+const postResponses = {
   '/signup': ({username, email}) => ({
     headers: {authorization: bearerToken},
     data: {
@@ -23,7 +22,16 @@ const responses = {
       },
     },
   }),
-  [`${baseUrl}/boards/1`]: {
+  '/me/boards': ({title}) => ({
+    data: {board: {id: 1, title, owner_id: 2}},
+  }),
+};
+
+const getResponses = {
+  '/me/boards': {
+    data: {boards: [{id: 1, title: 'hello board', owner_id: 2}]},
+  },
+  '/boards/1': {
     data: {board: {id: 1, title: 'hello board', owner_id: 2}},
   },
 };
@@ -40,14 +48,13 @@ class MockRequest {
 
   post(url, data) {
     return new Promise(resolve => {
-      const response = responses[url](data);
+      const response = postResponses[url](data);
       resolve(response);
     });
   }
   get(url) {
     return new Promise(resolve => {
-      console.log(url);
-      const response = responses[url];
+      const response = getResponses[url];
       resolve(response);
     });
   }
