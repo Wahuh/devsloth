54397f7113ea617f98a84288fc8aa3c0290b68ea
Thanh <tmdoan98@gmail.com>
2019-10-20 23:44:52 +0100

    add list_id and onGetTasks props

--- client/src/tasks/components/TasksList/index.jsx
+++ client/src/tasks/components/TasksList/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {selectTasksByListId} from '../../redux/selectors';
 import styles from './TasksList.module.scss';
 import TaskItem from '../TaskItem';
+import {getTasksRequest} from '../../redux/actions';
 
-const TasksList = ({tasks}) => {
+const TasksList = ({tasks, onGetTasks, list_id}) => {
+  useEffect(() => {
+    onGetTasks({list_id});
+  }, []);
   return (
     <ul className={styles.TasksList}>
       {tasks.map(task => (
@@ -19,10 +23,17 @@ TasksList.propTypes = {
   tasks: PropTypes.arrayOf(
     PropTypes.objectOf({id: PropTypes.number, title: PropTypes.string}),
   ).isRequired,
+  onGetTasks: PropTypes.func.isRequired,
+  list_id: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => ({
   tasks: selectTasksByListId(state, ownProps.list_id),
 });
 
-export default connect(mapStateToProps)(TasksList);
+export default connect(
+  mapStateToProps,
+  {
+    onGetTasks: getTasksRequest,
+  },
+)(TasksList);
