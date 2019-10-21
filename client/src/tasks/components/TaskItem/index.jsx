e69dc5a5df9204c783379cef9d4a325558841188
Thanh Doan <31378256+Wahuh@users.noreply.github.com>
2019-10-21 14:29:18 +0100

    add basic dnd (#32)
    * add basic drag & drop

--- client/src/tasks/components/TaskItem/index.jsx
+++ client/src/tasks/components/TaskItem/index.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import {Draggable} from 'react-beautiful-dnd';
 import PropTypes from 'prop-types';
 import styles from './TaskItem.module.scss';
 import Typography from '../../../common/components/Typography';
 
-const TaskItem = ({task}) => {
-  const {title} = task;
+const TaskItem = ({task, index}) => {
+  const {title, id} = task;
   return (
-    <li className={styles.TaskItem}>
-      <Typography as="p" color="secondary">
-        {title}
-      </Typography>
-    </li>
+    <Draggable draggableId={id.toString()} index={index}>
+      {provided => (
+        <li
+          /* eslint-disable */
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          /* eslint-enable */
+          ref={provided.innerRef}
+          className={styles.TaskItem}
+        >
+          <Typography as="p" color="secondary">
+            {title}
+          </Typography>
+        </li>
+      )}
+    </Draggable>
   );
 };
 
 TaskItem.propTypes = {
-  task: PropTypes.shape({title: PropTypes.string}).isRequired,
+  task: PropTypes.shape({title: PropTypes.string, id: PropTypes.number})
+    .isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default TaskItem;
