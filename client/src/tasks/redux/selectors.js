bc326b77568b5f3579a8aad83e0012f1665e4d0f
Thanh <tmdoan98@gmail.com>
2019-10-22 20:14:17 +0100

    refactor selectTasksByListId to sort by position

--- client/src/tasks/redux/selectors.js
+++ client/src/tasks/redux/selectors.js
@@ -1,13 +1,20 @@
 /* eslint-disable */
 export const selectTasksByListId = (state, list_id) => {
   const taskIds = state.tasks.allIds;
-  const tasks = taskIds.reduce((acc, task_id) => {
-    const task = state.tasks.byId[task_id];
-    if (task.list_id === +list_id) {
-      acc.push(task);
-    }
-    return acc;
-  }, []);
+  const tasks = taskIds
+    .reduce((acc, task_id) => {
+      const task = state.tasks.byId[task_id];
+      if (task.list_id === +list_id) {
+        acc.push(task);
+      }
+      return acc;
+    }, [])
+    .sort((a, b) => a.position - b.position);
   return tasks;
+};
+
+export const selectTasksCountByListId = (state, list_id) => {
+  const taskIds = state.tasks.allIds;
+  return taskIds.filter(id => state.tasks.byId[id].list_id === list_id).length;
 };
 /* eslint-enable */
