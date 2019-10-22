339534ae4307dc4fb0799a09b34a6ce341e95924
Thanh <tmdoan98@gmail.com>
2019-10-22 21:59:15 +0100

    include position key in post data

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,8 +2,8 @@ import {normalize} from 'normalizr';
 import http from './http.api';
 import schemas from './schemas';
 
-export const postTask = async ({title, list_id}) => {
-  const {data} = await http.post(`/lists/${list_id}/tasks`, {title});
+export const postTask = async ({list_id, ...rest}) => {
+  const {data} = await http.post(`/lists/${list_id}/tasks`, rest);
   return normalize(data.task, schemas.task);
 };
 
