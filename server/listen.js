ae6be029bf080365c858f45eaf9ef3295201f1bf
Thanh <tmdoan98@gmail.com>
2019-10-23 15:34:17 +0100

    add env variable for port and default to 3000

--- server/listen.js
+++ server/listen.js
@@ -1,5 +1,6 @@
 const ioServer = require('./io-server');
+const {port} = require('./config');
 
-ioServer.listen(3000, () => {
-  process.stdout.write('listening on port 3000');
+ioServer.listen(port, () => {
+  process.stdout.write('listening on port');
 });
