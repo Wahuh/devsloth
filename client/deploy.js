629a72c120c0f4dbec04eec3a7db911cc26d3abc
Thanh Doan <31378256+Wahuh@users.noreply.github.com>
2019-10-24 12:34:18 +0100

    add new deploy script using aws-sdk (#45)
    * set NODE_ENV to production for build script
    
    * change apiUrl if running in production
    
    * uninstall s3-deploy, install aws-sdk
    
    * add script to deploy dist files to s3

---
+++ client/deploy.js
@@ -0,0 +1,44 @@
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
+
+const path = require('path');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3({
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+});
+
+var fs = require('fs');
+
+function uploadToS3(bucketName, keyPrefix, filePath) {
+  // ex: /path/to/my-picture.png becomes my-picture.png
+  var fileName = path.basename(filePath);
+  var fileStream = fs.createReadStream(filePath);
+
+  // If you want to save to "my-bucket/{prefix}/{filename}"
+  //                    ex: "my-bucket/my-pictures-folder/my-picture.png"
+  var keyName = path.join(keyPrefix, fileName);
+
+  return new Promise(function(resolve, reject) {
+    fileStream.once('error', reject);
+    s3.upload(
+      {
+        Bucket: bucketName,
+        Key: keyName,
+        Body: fileStream,
+      },
+      function(err, result) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(result);
+      },
+    );
+  });
+}
+
+uploadToS3('www.slothy.io', '', './dist/main.js');
+uploadToS3('www.slothy.io', '', './dist/main.css');
+uploadToS3('www.slothy.io', '', './dist/index.html');
