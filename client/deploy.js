4a24eddcf56a39a0c737dd5ec3564bd5b0a0582a
Thanh Doan <31378256+Wahuh@users.noreply.github.com>
2019-10-24 13:10:02 +0100

    fix deploy script (#46)
    * use process.env.AWS_ACCESS_KEY_ID

--- client/deploy.js
+++ client/deploy.js
@@ -1,4 +1,4 @@
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID;
 var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY;
 
 const path = require('path');
